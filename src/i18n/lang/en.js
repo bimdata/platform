d922ce2a197c19183e5d6b07ce4625c7754d66e7
NicolasRichel <nicolas@bimdata.io>
2020-12-18 16:57:19 +0100

    feat(i18n): add i18n support

---
+++ src/i18n/lang/en.js
@@ -0,0 +1,11 @@
+export default {
+  Home: {
+    welcome: 'Welcome BIMData Platform !',
+    selectLocaleLabel: 'Choose a language',
+    logout: 'Log Out',
+  },
+  oidc: {
+    callbackMessage: 'OIDC callback message',
+    callbackErrorMessage: 'OIDC callbacl error message'
+  }
+};
