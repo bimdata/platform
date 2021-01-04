0bc916ce1235b0fc73cba91783168a679d43b23b
NicolasRichel <nicolas@bimdata.io>
2021-01-04 13:36:31 +0100

    feat(style): add spinner on OidcCallback view + rearrange css

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,9 @@
   <router-view />
 </template>
 
-<style lang="scss">
-@import '~@bimdata/design-system/dist/scss/utilities/spacing.scss';
-@import '~@bimdata/design-system/dist/scss/utilities/text.scss';
-
+<style>
 body {
   margin: 0;
   padding: 0;
-  min-width: 100vw;
-  min-height: 100vh;
-  background-color: $color-tertiary-lightest;
-  font-family: $primary-font;
-  font-size: $font-size;
 }
 </style>
--- src/views/oidc-callback/OidcCallback.vue
+++ src/views/oidc-callback/OidcCallback.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="oidc-callback-view">
-    OIDC Callback view
+    <BIMDataBigSpinner style="transform: translateX(130px);"></BIMDataBigSpinner>
   </div>
 </template>
 
@@ -8,17 +8,24 @@
 import { onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { useGlobalState } from '@/state/globalState';
+// Components
+import BIMDataBigSpinner from '@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataBigSpinner.js';
 
 export default {
+  components: {
+    BIMDataBigSpinner
+  },
   setup() {
     const router = useRouter();
     const { signInCallback } = useGlobalState();
 
     onMounted(() => {
-      signInCallback().then(result => {
-        router.push({ path: result.state ? result.state : '/' });
-      });
+      signInCallback().then(
+        result => router.push({ path: result.state ? result.state : '/' })
+      );
     });
   }
 }
 </script>
+
+<style scoped lang="scss" src="./OidcCallback.scss"></style>
