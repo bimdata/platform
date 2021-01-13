55c215ff9f827d400d8eb283eb533163d1ca2cc8
NicolasRichel <nicolas@bimdata.io>
2021-01-13 17:22:37 +0100

    feat(breadcrumb): create breadcrumb component

---
+++ src/components/platform-breadcrumb/PlatformBreadcrumb.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="platform-breadcrumb">
+    <!-- TODO -->
+  </div>
+</template>
+
+<script>
+export default {
+  // TODO
+}
+</script>
+
+<style scoped lang="scss" src="./PlatformBreadcrumb.scss"></style>
