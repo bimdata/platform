2c210272e91e4583623cfe72f54fe48cedde53f7
NicolasRichel <nicolas@bimdata.io>
2021-01-26 09:17:40 +0100

    feat(projects): create projects view component

---
+++ src/views/projects/Projects.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="projects-view">
+    <!-- TODO -->
+  </div>
+</template>
+
+<script>
+export default {
+  // TODO
+}
+</script>
+
+<style scoped lang="scss" src="./Projects.scss"></style>
