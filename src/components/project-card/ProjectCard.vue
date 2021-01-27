1f212156b8f32134097b5d1de5ed222ba3151627
NicolasRichel <nicolas@bimdata.io>
2021-01-27 10:59:18 +0100

    feat(projects): create base components to compose projects view

---
+++ src/components/project-card/ProjectCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <BIMDataCard class="project-card">
+    <!-- TODO -->
+    <template #content>
+      Project ID: {{ project.id }}<br/>
+      Project Name: {{ project.name }}
+    </template>
+  </BIMDataCard>
+</template>
+
+<script>
+import BIMDataCard from '@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataCard.js';
+
+export default {
+  components: {
+    BIMDataCard,
+  },
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./ProjectCard.scss"></style>
