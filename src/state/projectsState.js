b41b5ca85cc024fc36c6c3e46e57c1c0231fcf94
NicolasRichel <nicolas@bimdata.io>
2021-01-27 10:56:48 +0100

    feat(projects): add projects state

---
+++ src/state/projectsState.js
@@ -0,0 +1,50 @@
+import { reactive, readonly, toRefs } from 'vue';
+import ProjectsService from '@/api/ProjectsService';
+
+const state = reactive({
+  projects: [],
+  currentProject: null,
+});
+
+const fetchProjects = (space) => {
+  ProjectsService.fetchSpaceProjects(space).then(
+    projects => state.projects = projects
+  );
+};
+
+const createProject = (space, project) => {
+  ProjectsService.createProject(space, project).then(
+    newProject => state.projects = [newProject].concat(state.projects)
+  );
+};
+
+const updateProject = (space, project) => {
+  ProjectsService.updateProject(space, project).then(softUpdateProject);
+};
+
+const softUpdateProject = (project) => {
+  state.projects = state.projects.map(p => p.id === project.id ? project : p);
+};
+
+const deleteProject = (space, project) => {
+  ProjectsService.deleteProject(space, project).then(
+    () => state.projects = state.projects.filter(p => p.id !== project.id)
+  );
+};
+
+const selectProject = (id) => {
+  state.currentProject = state.projects.find(p => p.id === id) || null;
+};
+
+export function useProjectsState() {
+  const readonlyState = readonly(state);
+  return {
+    ...toRefs(readonlyState),
+    fetchProjects,
+    createProject,
+    updateProject,
+    softUpdateProject,
+    deleteProject,
+    selectProject,
+  };
+}
--- src/state/spacesState.js
+++ src/state/spacesState.js
@@ -2,32 +2,43 @@ import { reactive, readonly, toRefs } from 'vue';
 import SpacesService from '@/api/SpacesService';
 
 const state = reactive({
-  spaces: []
+  spaces: [],
+  currentSpace: null,
 });
 
-const fetchSpaces = () => SpacesService.fetchUserSpaces().then(
-  spaces => state.spaces = spaces
-);
+const fetchSpaces = () => {
+  SpacesService.fetchUserSpaces().then(
+    spaces => state.spaces = spaces
+  );
+};
 
-const createSpace = (space) => SpacesService.createSpace(space).then(
-  createdSpace => state.spaces = [createdSpace].concat(state.spaces)
-);
+const createSpace = (space) => {
+  SpacesService.createSpace(space).then(
+    newSpace => state.spaces = [newSpace].concat(state.spaces)
+  );
+};
 
-const updateSpace = (space) => SpacesService.updateSpace(space).then(
-  softUpdateSpace
-);
+const updateSpace = (space) => {
+  SpacesService.updateSpace(space).then(softUpdateSpace);
+};
 
-const softUpdateSpace = (space) => state.spaces = state.spaces.map(
-  s => s.id === space.id ? space : s
-);
+const softUpdateSpace = (space) => {
+  state.spaces = state.spaces.map(s => s.id === space.id ? space : s);
+};
 
-const removeSpaceImage = (space) => SpacesService.removeSpaceImage(space).then(
-  softUpdateSpace
-);
+const removeSpaceImage = (space) => {
+  SpacesService.removeSpaceImage(space).then(softUpdateSpace);
+};
 
-const deleteSpace = (space) => SpacesService.deleteSpace(space).then(
-  () => state.spaces = state.spaces.filter(s => s.id !== space.id)
-);
+const deleteSpace = (space) => {
+  SpacesService.deleteSpace(space).then(
+    () => state.spaces = state.spaces.filter(s => s.id !== space.id)
+  );
+};
+
+const selectSpace = (id) => {
+  state.currentSpace = state.spaces.find(s => s.id === id) || null;
+};
 
 export function useSpacesState() {
   const readonlyState = readonly(state);
@@ -39,5 +50,6 @@ export function useSpacesState() {
     softUpdateSpace,
     removeSpaceImage,
     deleteSpace,
+    selectSpace,
   };
 }
