231e9b3a414e818c942488e8d90b8fc9c11a0b91
NicolasRichel <nicolas@bimdata.io>
2021-01-28 22:49:26 +0100

    feat(projects): create project card structure

---
+++ src/components/project-action-bar/ProjectActionBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="project-action-bar">
+    <BIMDataButton ghost rounded icon>
+      <BIMDataIcon name="show" size="s" />
+    </BIMDataButton>
+    <BIMDataButton ghost rounded icon>
+      <BIMDataIcon name="ellipsis" size="l" />
+    </BIMDataButton>
+  </div>
+</template>
+
+<script>
+import BIMDataButton from '@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataButton.js';
+import BIMDataIcon from '@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataIcon.js';
+
+export default {
+  components: {
+    BIMDataButton,
+    BIMDataIcon,
+  },
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./ProjectActionBar.scss"></style>
