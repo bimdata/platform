1c90dc2d290b1caf3e901d7e01bac9ece4510651
NicolasRichel <nicolas@bimdata.io>
2021-01-28 09:15:38 +0100

    fix(#18): navigate to dashboard on header-logo click

--- src/components/platform-header/PlatformHeader.vue
+++ src/components/platform-header/PlatformHeader.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="platform-header">
     <img
+      class="platform-header__logo"
       alt="BIMData Platform logo"
       src="@/assets/header-logo.svg"
       width="110"
       height="50"
+      @click="goToDashboard"
     />
-    <UserMenu />
+    <UserMenu
+      class="platform-header__user-menu"
+    />
   </div>
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
 // Components
 import UserMenu from '@/components/user-menu/UserMenu';
 
 export default {
   components: {
     UserMenu
+  },
+  setup() {
+    const router = useRouter();
+
+    const goToDashboard = () => {
+      router.push({ name: 'dashboard' });
+    };
+
+    return {
+      goToDashboard
+    };
   }
 }
 </script>
