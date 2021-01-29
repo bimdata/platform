48893c9e75b54d15a6a64dc3aada2225bcef3798
NicolasRichel <nicolas@bimdata.io>
2021-01-29 14:42:07 +0100

    feat(projects): search/sort projects feature

--- src/components/go-back-button/GoBackButton.vue
+++ src/components/go-back-button/GoBackButton.vue
@@ -1,14 +1,13 @@
 <template>
   <BIMDataButton ghost radius
     class="go-back-button"
-    @click="goBack">
-    <BIMDataIcon name="arrow" size="xxxs" />
-    <span>{{ $t('Header.back') }}</span>
+    @click="$router.back()">
+    <BIMDataIcon name="arrow" size="xxs" />
+    <span>{{ $t('Breadcrumb.back') }}</span>
   </BIMDataButton>
 </template>
 
 <script>
-import { useRouter } from 'vue-router';
 // Components
 import BIMDataButton from '@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataButton.js';
 import BIMDataIcon from '@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataIcon.js';
@@ -17,13 +16,8 @@ export default {
   components: {
     BIMDataButton,
     BIMDataIcon
-  },
-  setup() {
-    const router = useRouter();
-    const goBack = () => router.back();
-    return {
-      goBack
-    };
   }
 }
 </script>
+
+<style scoped lang="scss" src="./GoBackButton.scss"></style>
