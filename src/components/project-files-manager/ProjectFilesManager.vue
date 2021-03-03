d7e8cb43f6ebde71db60b44cd58aa1f78d5200fb
NicolasRichel <nicolas@bimdata.io>
2021-03-03 10:56:19 +0100

    feat(project-board): extract models, users and files manager in seperate components

---
+++ src/components/project-files-manager/ProjectFilesManager.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="project-files-manager">
+    <img src="/static/empty-project-illustration-files.svg" />
+    <div>Téléverser votre premier fichier</div>
+    <div class="project-files-manager__actions">
+      <BIMDataButton color="primary" fill radius>
+        Téléversement
+      </BIMDataButton>
+      <BIMDataButton color="primary" fill radius>
+        Créer un dossier
+      </BIMDataButton>
+    </div>
+  </div>
+</template>
+
+<script>
+// Components
+import BIMDataButton from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataButton.js";
+
+export default {
+  components: {
+    BIMDataButton
+  },
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  },
+  setup() {
+    // TODO
+  }
+};
+</script>
+
+<style scoped lang="scss" src="./ProjectFilesManager.scss"></style>
---
+++ src/components/project-users-manager/ProjectUsersManager.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="project-users-manager">
+    <img src="/static/empty-project-illustration-users.svg" />
+    <div>Inviter votre premier collaborateur</div>
+    <div class="project-users-manager__actions">
+      <BIMDataButton color="primary" fill radius>
+        Invitation
+      </BIMDataButton>
+    </div>
+  </div>
+</template>
+
+<script>
+// Components
+import BIMDataButton from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataButton.js";
+
+export default {
+  components: {
+    BIMDataButton
+  },
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  },
+  setup() {
+    // TODO
+  }
+};
+</script>
+
+<style scoped lang="scss" src="./ProjectUsersManager.scss"></style>
