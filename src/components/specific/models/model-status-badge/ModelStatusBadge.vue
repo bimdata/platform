fd22a0b0e4fbce4048ca6111575c673d6e27a9bc
NicolasRichel <nicolas@bimdata.io>
2021-03-18 14:21:57 +0100

    feat(models-manager): create model-status-badge and model-action-menu

---
+++ src/components/specific/models/model-status-badge/ModelStatusBadge.vue
@@ -0,0 +1,53 @@
+<template>
+  <span class="model-status-badge" :class="`model-status-badge--${statusName}`">
+    <BIMDataIcon :name="statusIcon" size="s" />
+  </span>
+</template>
+
+<script>
+import { ref } from "vue";
+// Components
+import BIMDataIcon from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataIcon.js";
+
+export default {
+  components: {
+    BIMDataIcon
+  },
+  props: {
+    status: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const statusName = ref("");
+    const statusIcon = ref("");
+
+    switch (props.status) {
+      case "P":
+        statusName.value = "pending";
+        statusIcon.value = "success";
+        break;
+      case "I":
+        statusName.value = "in-progress";
+        statusIcon.value = "refresh";
+        break;
+      case "C":
+        statusName.value = "completed";
+        statusIcon.value = "success";
+        break;
+      case "E":
+        statusName.value = "error";
+        statusIcon.value = "failed";
+        break;
+    }
+
+    return {
+      statusIcon,
+      statusName
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss" src="./ModelStatusBadge.scss"></style>
--- src/components/specific/models/project-models-manager/ProjectModelsManager.vue
+++ src/components/specific/models/project-models-manager/ProjectModelsManager.vue
@@ -13,20 +13,29 @@
       />
 
       <GenericTable
+        class="project-models-manager__table"
         :columns="columns"
         :rows="rows"
         :selectable="true"
         @selection-change="() => {}"
       >
-        <template #cell-version>0</template>
+        <template #cell-version>?</template>
         <template #cell-creator="{ row: { creator } }">
           {{ creator ? `${creator.firstname} ${creator.lastname[0]}.` : "" }}
         </template>
-        <template #cell-lastupdate="{ row: model }">
-          {{ model.updatedAt.toISOString().slice(0, -5) }}
+        <template #cell-lastupdate="{ row: { updatedAt } }">
+          {{
+            updatedAt
+              .toISOString()
+              .slice(0, -5)
+              .replace("T", " at ")
+          }}
         </template>
-        <template #cell-status="{ row: model }">
-          {{ model.status }}
+        <template #cell-status="{ row: { status } }">
+          <ModelStatusBadge :status="status" />
+        </template>
+        <template #cell-actions="{ row: model }">
+          <ModelActionMenu :model="model" />
         </template>
       </GenericTable>
     </template>
@@ -39,13 +48,16 @@ import { ref, watchEffect } from "vue";
 import BIMDataCard from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataCard.js";
 import BIMDataTabs from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataTabs.js";
 import GenericTable from "@/components/generic/generic-table/GenericTable";
+import ModelActionMenu from "@/components/specific/models/model-action-menu/ModelActionMenu";
+import ModelStatusBadge from "@/components/specific/models/model-status-badge/ModelStatusBadge";
 
 export default {
   components: {
     BIMDataCard,
-    // BIMDataTable,
     BIMDataTabs,
-    GenericTable
+    GenericTable,
+    ModelActionMenu,
+    ModelStatusBadge
   },
   props: {
     project: {
@@ -66,12 +78,13 @@ export default {
     ];
 
     const columns = [
-      { id: "id", label: "ID" },
+      { id: "id", label: "ID", width: "100px", align: "center" },
       { id: "name", label: "Nom" },
-      { id: "version", label: "Version" },
-      { id: "creator", label: "Creator" },
-      { id: "lastupdate", label: "Modifié le" },
-      { id: "status", label: "Statut" }
+      { id: "version", label: "Version", align: "center" },
+      { id: "creator", label: "Creator", align: "center" },
+      { id: "lastupdate", label: "Modifié le", align: "center" },
+      { id: "status", label: "Statut", align: "center" },
+      { id: "actions", label: " ", width: "300px" }
     ];
     const rows = ref([]);
     watchEffect(() => {
