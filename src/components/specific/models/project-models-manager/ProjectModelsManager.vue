2fe56b6b0e27ce0370b34831ba764ee0a8984b21
NicolasRichel <nicolas@bimdata.io>
2021-03-18 15:23:31 +0100

    feat(i18n): make tabs and table columns labels translatable

--- src/components/specific/models/project-models-manager/ProjectModelsManager.vue
+++ src/components/specific/models/project-models-manager/ProjectModelsManager.vue
@@ -12,38 +12,41 @@
         :tabs="tabs"
       />
 
-      <GenericTable
-        class="project-models-manager__table"
-        :columns="columns"
-        :rows="rows"
-        :selectable="true"
-        @selection-change="() => {}"
-      >
-        <template #cell-version>?</template>
-        <template #cell-creator="{ row: { creator } }">
-          {{ creator ? `${creator.firstname} ${creator.lastname[0]}.` : "" }}
-        </template>
-        <template #cell-lastupdate="{ row: { updatedAt } }">
-          {{
-            updatedAt
-              .toISOString()
-              .slice(0, -5)
-              .replace("T", " at ")
-          }}
-        </template>
-        <template #cell-status="{ row: { status } }">
-          <ModelStatusBadge :status="status" />
-        </template>
-        <template #cell-actions="{ row: model }">
-          <ModelActionMenu :model="model" />
-        </template>
-      </GenericTable>
+      <div class="project-models-manager__table-container">
+        <GenericTable
+          :columns="columns"
+          :rows="rows"
+          :selectable="true"
+          @selection-change="() => {}"
+        >
+          <template #cell-version>?</template>
+          <template #cell-creator="{ row: { creator } }">
+            {{ creator ? `${creator.firstname} ${creator.lastname[0]}.` : "" }}
+          </template>
+          <template #cell-lastupdate="{ row: { updatedAt } }">
+            {{
+              updatedAt
+                .toISOString()
+                .slice(0, -5)
+                .replace(/-/g, "/")
+                .replace("T", ` ${$t("Commons.at")} `)
+            }}
+          </template>
+          <template #cell-status="{ row: { status } }">
+            <ModelStatusBadge :status="status" />
+          </template>
+          <template #cell-actions="{ row: model }">
+            <ModelActionMenu :model="model" />
+          </template>
+        </GenericTable>
+      </div>
     </template>
   </BIMDataCard>
 </template>
 
 <script>
 import { ref, watchEffect } from "vue";
+import { useI18n } from "vue-i18n";
 // Components
 import BIMDataCard from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataCard.js";
 import BIMDataTabs from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataTabs.js";
@@ -70,22 +73,71 @@ export default {
     }
   },
   setup(props) {
-    const tabs = [
-      { id: "ifc", label: "IFC" },
-      { id: "split", label: "Split / Export" },
-      { id: "merge", label: "Merge" },
-      { id: "archive", label: "Archives" }
-    ];
+    const { t } = useI18n();
 
-    const columns = [
-      { id: "id", label: "ID", width: "100px", align: "center" },
-      { id: "name", label: "Nom" },
-      { id: "version", label: "Version", align: "center" },
-      { id: "creator", label: "Creator", align: "center" },
-      { id: "lastupdate", label: "Modifié le", align: "center" },
-      { id: "status", label: "Statut", align: "center" },
-      { id: "actions", label: " ", width: "300px" }
-    ];
+    const tabs = ref([]);
+    watchEffect(() => {
+      tabs.value = [
+        {
+          id: "ifc",
+          label: t("ProjectBoard.ProjectModelsManager.tabs.ifc")
+        },
+        {
+          id: "split",
+          label: t("ProjectBoard.ProjectModelsManager.tabs.split")
+        },
+        {
+          id: "merge",
+          label: t("ProjectBoard.ProjectModelsManager.tabs.merge")
+        },
+        {
+          id: "archive",
+          label: t("ProjectBoard.ProjectModelsManager.tabs.archive")
+        }
+      ];
+    });
+
+    const columns = ref([]);
+    watchEffect(() => {
+      columns.value = [
+        {
+          id: "id",
+          label: t("ProjectBoard.ProjectModelsManager.table.id"),
+          width: "100px",
+          align: "center"
+        },
+        {
+          id: "name",
+          label: t("ProjectBoard.ProjectModelsManager.table.name")
+        },
+        {
+          id: "version",
+          label: t("ProjectBoard.ProjectModelsManager.table.version"),
+          align: "center"
+        },
+        {
+          id: "creator",
+          label: t("ProjectBoard.ProjectModelsManager.table.creator"),
+          align: "center"
+        },
+        {
+          id: "lastupdate",
+          label: t("ProjectBoard.ProjectModelsManager.table.lastupdate"),
+          align: "center"
+        },
+        {
+          id: "status",
+          label: t("ProjectBoard.ProjectModelsManager.table.status"),
+          align: "center"
+        },
+        {
+          id: "actions",
+          label: " ",
+          width: "300px"
+        }
+      ];
+    });
+
     const rows = ref([]);
     watchEffect(() => {
       rows.value = props.models;
