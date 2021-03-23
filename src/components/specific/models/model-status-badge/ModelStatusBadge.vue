0de5e9937b8275409226959194bee14f751796b7
NicolasRichel <nicolas@bimdata.io>
2021-03-23 09:29:56 +0100

    fix(models-manager): remove warnings/errors tooltip from model-status-badge

--- src/components/specific/models/model-status-badge/ModelStatusBadge.vue
+++ src/components/specific/models/model-status-badge/ModelStatusBadge.vue
@@ -1,32 +1,7 @@
 <template>
   <span class="model-status-badge" :class="`model-status-badge--${statusName}`">
-    <BIMDataIcon :name="statusIcon" size="s" />
-
-    <div
-      v-if="statusName === 'warning'"
-      class="model-status-badge__tooltip model-status-badge__tooltip--warnings"
-    >
-      <div
-        v-for="(warning, i) in warnings"
-        :key="`warning-${i}`"
-        class="model-status-badge__tooltip__message"
-      >
-        {{ warning }}
-      </div>
-    </div>
-
-    <div
-      v-if="statusName === 'error'"
-      class="model-status-badge__tooltip model-status-badge__tooltip--errors"
-    >
-      <div
-        v-for="(error, i) in errors"
-        :key="`error-${i}`"
-        class="model-status-badge__tooltip__message"
-      >
-        {{ error }}
-      </div>
-    </div>
+    <BIMDataSpinner v-if="['pending', 'in-progress'].includes(statusName)" />
+    <BIMDataIcon v-else :name="statusIcon" size="s" />
   </span>
 </template>
 
@@ -34,10 +9,12 @@
 import { ref, watchEffect } from "vue";
 // Components
 import BIMDataIcon from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataIcon.js";
+import BIMDataSpinner from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataSpinner.js";
 
 export default {
   components: {
-    BIMDataIcon
+    BIMDataIcon,
+    BIMDataSpinner
   },
   props: {
     model: {
@@ -48,42 +25,29 @@ export default {
   setup(props) {
     const statusName = ref("");
     const statusIcon = ref("");
-    const warnings = ref([]);
-    const errors = ref([]);
 
     watchEffect(() => {
       switch (props.model.status) {
         case "P":
           statusName.value = "pending";
-          statusIcon.value = "success";
           break;
         case "I":
           statusName.value = "in-progress";
-          statusIcon.value = "refresh";
           break;
         case "C":
-          if (props.model.warnings.length > 0) {
-            statusName.value = "warning";
-            statusIcon.value = "warning";
-            warnings.value = props.model.warnings;
-          } else {
-            statusName.value = "completed";
-            statusIcon.value = "success";
-          }
+          statusName.value = "completed";
+          statusIcon.value = "success";
           break;
         case "E":
           statusName.value = "error";
           statusIcon.value = "failed";
-          errors.value = props.model.errors;
           break;
       }
     });
 
     return {
-      errors,
       statusIcon,
-      statusName,
-      warnings
+      statusName
     };
   }
 };
