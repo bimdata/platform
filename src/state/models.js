5d660576f5df3fd2f608afee11c5d265f593ed09
NicolasRichel <nicolas@bimdata.io>
2021-03-23 09:01:54 +0100

    feat(model-location): add the ability to define an IFC Site on model (when it doesn't exist)

--- src/state/models.js
+++ src/state/models.js
@@ -1,17 +1,31 @@
 import { reactive, readonly, toRefs } from "vue";
-import ModelService from "@/server/IfcService";
+import ModelService from "@/server/ModelService";
 
 const state = reactive({
   projectModels: []
 });
 
 const loadProjectModels = async project => {
-  state.projectModels = await ModelService.fetchProjectIfcs(project);
-  return state.projectModels;
+  const models = await ModelService.fetchProjectModels(project);
+  state.projectModels = models;
+  return models;
+};
+
+const updateModel = async (project, model) => {
+  const newModel = await ModelService.updateModel(project, model);
+  softUpdateModel(newModel);
+  return newModel;
+};
+
+const softUpdateModel = model => {
+  state.projectModels = state.projectModels.map(m =>
+    m.id === model.id ? { ...m, ...model } : m
+  );
+  return model;
 };
 
 const fetchModelSites = async (project, model) => {
-  const sites = await ModelService.fetchIfcElementsByType(
+  const sites = await ModelService.fetchModelElementsByType(
     project,
     model,
     "IfcSite"
@@ -19,11 +33,65 @@ const fetchModelSites = async (project, model) => {
   return sites;
 };
 
+const setModelSite = async (
+  project,
+  model,
+  { address, longitude, latitude }
+) => {
+  const site = {
+    type: "IfcSite",
+    attributes: {
+      type: "Attributes",
+      properties: [
+        {
+          definition: { name: "SiteAddress" },
+          value: address
+        },
+        {
+          definition: { name: "RefLongitude" },
+          value: longitude
+        },
+        {
+          definition: { name: "RefLatitude" },
+          value: latitude
+        }
+      ]
+    }
+  };
+  const newSite = await ModelService.createModelElements(project, model, site);
+  return newSite;
+};
+
+const updateModelSite = async (
+  project,
+  model,
+  site,
+  { address, longitude, latitude }
+) => {
+  const properties = [
+    { name: "SiteAddress", value: address },
+    { name: "RefLongitude", value: longitude },
+    { name: "RefLatitude", value: latitude }
+  ];
+  const newProperties = await ModelService.setModelElementAttributesProperties(
+    project,
+    model,
+    site,
+    properties,
+    { update: true }
+  );
+  return newProperties;
+};
+
 export function useModels() {
   const readonlyState = readonly(state);
   return {
     ...toRefs(readonlyState),
     loadProjectModels,
-    fetchModelSites
+    updateModel,
+    softUpdateModel,
+    fetchModelSites,
+    setModelSite,
+    updateModelSite
   };
 }
