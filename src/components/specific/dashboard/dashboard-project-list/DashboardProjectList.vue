753328c9b9c220b4d2fb2639dea74cdf16ab7b70
NicolasRichel <nicolas@bimdata.io>
2021-04-12 13:07:12 +0200

    fix(style): fix dashboard elements spacing

--- src/components/specific/dashboard/dashboard-project-list/DashboardProjectList.vue
+++ src/components/specific/dashboard/dashboard-project-list/DashboardProjectList.vue
@@ -3,24 +3,26 @@
     <div class="dashboard-project-list__title">
       {{ $t("DashboardProjectList.title") }}
     </div>
-    <div class="dashboard-project-list__content">
+    <CarouselList>
       <ProjectCard
         v-for="project in displayedProjects"
         :key="project.id"
         :project="project"
         :actionMenu="false"
       />
-    </div>
+    </CarouselList>
   </div>
 </template>
 
 <script>
 import { ref, watchEffect } from "vue";
 // Components
+import CarouselList from "@/components/generic/carousel-list/CarouselList";
 import ProjectCard from "@/components/specific/projects/project-card/ProjectCard";
 
 export default {
   components: {
+    CarouselList,
     ProjectCard
   },
   props: {
@@ -37,7 +39,7 @@ export default {
         displayedProjects.value = props.projects
           .slice()
           .sort((a, b) => (a.updatedAt < b.updatedAt ? 1 : -1))
-          .slice(0, 4);
+          .slice(0, 5);
       }
     });
 
