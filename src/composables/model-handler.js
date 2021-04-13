0980fdce1526a9c70aba7a5e676d2343b6819538
NicolasRichel <nicolas@bimdata.io>
2021-04-13 22:39:50 +0200

    chore(clean): remove useless ModelHandler class

--- src/composables/model-handler.js
+++
@@ -1,7 +0,0 @@
-import ModelHandler from "@/server/ModelHandler";
-
-export function useModelHandler(model) {
-  const modelHandler = new ModelHandler();
-  modelHandler.load(model);
-  return modelHandler;
-}
