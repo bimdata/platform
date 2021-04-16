d1cbdf9a81218805243ffc5972b61e183360a793
NicolasRichel <nicolas@bimdata.io>
2021-04-16 09:23:41 +0200

    chore(i18n): explicitly disable legacy mode

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,8 +3,14 @@ import en from "./lang/en";
 import fr from "./lang/fr";
 
 const i18n = createI18n({
+  // Disable legacy mode and use composition API instead
+  legacy: false,
+  // Makes i18n functions ($i18n, $t, $d, ...) available in all components templates
+  globalInjection: true,
+  // Default & fallback locales
   locale: "fr",
   fallbackLocale: "en",
+
   messages: {
     en,
     fr
