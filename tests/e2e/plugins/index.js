1c88a27d506108623efb64b09549070747ce945d
NicolasRichel <nicolas@bimdata.io>
2021-04-27 15:59:46 +0200

    test(e2e): setup cypress for e2e testing

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,25 +1,15 @@
-/* eslint-disable arrow-body-style */
+// ***********************************************
+// See documentation for more info:
 // https://docs.cypress.io/guides/guides/plugins-guide.html
-
-// if you need a custom webpack configuration you can uncomment the following import
-// and then use the `file:preprocessor` event
-// as explained in the cypress docs
-// https://docs.cypress.io/api/plugins/preprocessors-api.html#Examples
-
-// /* eslint-disable import/no-extraneous-dependencies, global-require */
-// const webpack = require('@cypress/webpack-preprocessor')
+// ***********************************************
 
 module.exports = (on, config) => {
-  // on('file:preprocessor', webpack({
-  //  webpackOptions: require('@vue/cli-service/webpack.config'),
-  //  watchOptions: {}
-  // }))
-
   return Object.assign({}, config, {
-    fixturesFolder: "tests/e2e/fixtures",
     integrationFolder: "tests/e2e/specs",
+    fixturesFolder: "tests/e2e/fixtures",
+    supportFile: "tests/e2e/support/index.js",
+    downloadsFolder: "tests/e2e/downloads",
     screenshotsFolder: "tests/e2e/screenshots",
-    videosFolder: "tests/e2e/videos",
-    supportFile: "tests/e2e/support/index.js"
+    videosFolder: "tests/e2e/videos"
   });
 };
--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -1,25 +1,24 @@
 // ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
+// See documentation for more info:
+// https://docs.cypress.io/api/cypress-api/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", () => {
+  const key =
+    "user:https://iam-staging.bimdata.io/auth/realms/bimdata:ab5c58fa-2765-4e8b-be9f-c537c0c46d81";
+  const user = {
+    id_token: "id-token",
+    access_token: "access-token",
+    refresh_token: "refresh-token",
+    profile: {},
+    scope: "openid profile email"
+
+    // Unused fields
+    // expires_at: null,
+    // session_state: null,
+    // state: null,
+    // token_type: null,
+  };
+
+  localStorage.setItem(key, JSON.stringify(user));
+});
