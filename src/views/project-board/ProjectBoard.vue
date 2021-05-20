b7fbccf153cb11a7f0028b8ac98a2c7c6feabf97
NicolasRichel <nicolas@bimdata.io>
2021-05-20 09:24:27 +0200

    refactor(project-board): extract project-board content into project-overview sub-views

--- src/views/project-board/ProjectBoard.vue
+++ src/views/project-board/ProjectBoard.vue
@@ -15,54 +15,12 @@
       </template>
     </ViewHeader>
 
-    <div class="project-board__container">
-      <app-slot-content name="view-header-right">
-        <BIMDataButton
-          data-test="btn-toggle-upload"
-          width="120px"
-          :color="showFileUploader ? 'tertiary-darkest' : 'primary'"
-          fill
-          radius
-          @click="toggleFileUploader"
-        >
-          <BIMDataIcon
-            :name="showFileUploader ? 'close' : 'plus'"
-            size="xxxs"
-          />
-          <span>{{
-            showFileUploader
-              ? $t("ProjectBoard.closeFileUploadButtonText")
-              : $t("ProjectBoard.openFileUploadButtonText")
-          }}</span>
-        </BIMDataButton>
-      </app-slot-content>
-
-      <transition name="fade">
-        <FileUploader
-          v-show="showFileUploader"
-          class="project-board__container__block--upload"
-          :project="project"
-          :allowedFileTypes="['.ifc', '.ifczip']"
-          @file-uploaded="reloadModels"
-          @close="closeFileUploader"
-        />
-      </transition>
-      <ModelsOverview
-        class="project-board__container__block--overview"
+    <div class="project-board__body">
+      <ProjectOverview
         :project="project"
         :models="models"
-        @open-file-uploader="openFileUploader"
-      />
-      <ProjectUsersManager
-        class="project-board__container__block--users"
-        :project="project"
         :users="users"
         :invitations="invitations"
-      />
-      <ModelsManager
-        class="project-board__container__block--models"
-        :project="project"
-        :models="models"
       />
     </div>
   </div>
@@ -74,35 +32,23 @@ import { useI18n } from "vue-i18n";
 import { useModels } from "@/state/models";
 import { useProjects } from "@/state/projects";
 // Components
-import BIMDataButton from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataButton.js";
-import BIMDataIcon from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataIcon.js";
 import BIMDataTabs from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataTabs.js";
-import AppSlotContent from "@/components/generic/app-slot-content/AppSlotContent.vue";
 import ViewHeader from "@/components/generic/view-header/ViewHeader";
 import AppBreadcrumb from "@/components/specific/app/app-breadcrumb/AppBreadcrumb";
-import FileUploader from "@/components/specific/files/file-uploader/FileUploader";
-import ModelsManager from "@/components/specific/models/models-manager/ModelsManager";
-import ModelsOverview from "@/components/specific/models/models-overview/ModelsOverview";
-import ProjectUsersManager from "@/components/specific/users/project-users-manager/ProjectUsersManager";
+import ProjectOverview from "./project-overview/ProjectOverview";
 
 export default {
   components: {
-    BIMDataButton,
-    BIMDataIcon,
     BIMDataTabs,
-    AppSlotContent,
     ViewHeader,
     AppBreadcrumb,
-    FileUploader,
-    ModelsManager,
-    ModelsOverview,
-    ProjectUsersManager
+    ProjectOverview
   },
   setup() {
     const { t } = useI18n();
     const { currentProject, currentProjectUsers, currentProjectInvitations } =
       useProjects();
-    const { projectModels, loadProjectModels } = useModels();
+    const { projectModels } = useModels();
 
     const tabs = ref([]);
     watchEffect(() => {
@@ -113,38 +59,13 @@ export default {
       ];
     });
 
-    let reloadDebounce = null;
-    const reloadModels = () => {
-      clearTimeout(reloadDebounce);
-      reloadDebounce = setTimeout(async () => {
-        await loadProjectModels(currentProject.value);
-      }, 1000);
-    };
-
-    const showFileUploader = ref(false);
-    const openFileUploader = () => {
-      showFileUploader.value = true;
-    };
-    const closeFileUploader = () => {
-      showFileUploader.value = false;
-    };
-    const toggleFileUploader = () => {
-      showFileUploader.value = !showFileUploader.value;
-    };
-
     return {
       // References
       invitations: currentProjectInvitations,
       models: projectModels,
       project: currentProject,
-      showFileUploader,
       tabs,
-      users: currentProjectUsers,
-      // Methods
-      closeFileUploader,
-      openFileUploader,
-      reloadModels,
-      toggleFileUploader
+      users: currentProjectUsers
     };
   }
 };
---
+++ src/views/project-board/project-overview/ProjectOverview.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="project-overview">
+    <app-slot-content name="view-header-right">
+      <BIMDataButton
+        data-test="btn-toggle-upload"
+        width="120px"
+        :color="showFileUploader ? 'tertiary-darkest' : 'primary'"
+        fill
+        radius
+        @click="toggleFileUploader"
+      >
+        <BIMDataIcon :name="showFileUploader ? 'close' : 'plus'" size="xxxs" />
+        <span>{{
+          showFileUploader
+            ? $t("ProjectBoard.closeFileUploadButtonText")
+            : $t("ProjectBoard.openFileUploadButtonText")
+        }}</span>
+      </BIMDataButton>
+    </app-slot-content>
+
+    <transition name="fade">
+      <FileUploader
+        v-show="showFileUploader"
+        class="project-overview__block--upload"
+        :project="project"
+        :allowedFileTypes="['.ifc', '.ifczip']"
+        @file-uploaded="reloadModels"
+        @close="closeFileUploader"
+      />
+    </transition>
+    <ModelsOverview
+      class="project-overview__block--overview"
+      :project="project"
+      :models="models"
+      @open-file-uploader="openFileUploader"
+    />
+    <ProjectUsersManager
+      class="project-overview__block--users"
+      :project="project"
+      :users="users"
+      :invitations="invitations"
+    />
+    <ModelsManager
+      class="project-overview__block--models"
+      :project="project"
+      :models="models"
+    />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useModels } from "@/state/models";
+// Components
+import BIMDataButton from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataButton.js";
+import BIMDataIcon from "@bimdata/design-system/dist/js/BIMDataComponents/vue3/BIMDataIcon.js";
+import AppSlotContent from "@/components/generic/app-slot-content/AppSlotContent.vue";
+import FileUploader from "@/components/specific/files/file-uploader/FileUploader";
+import ModelsManager from "@/components/specific/models/models-manager/ModelsManager";
+import ModelsOverview from "@/components/specific/models/models-overview/ModelsOverview";
+import ProjectUsersManager from "@/components/specific/users/project-users-manager/ProjectUsersManager";
+
+export default {
+  components: {
+    BIMDataButton,
+    BIMDataIcon,
+    AppSlotContent,
+    FileUploader,
+    ModelsManager,
+    ModelsOverview,
+    ProjectUsersManager
+  },
+  props: {
+    project: {
+      type: Object,
+      required: true
+    },
+    models: {
+      type: Array,
+      required: true
+    },
+    users: {
+      type: Array,
+      required: true
+    },
+    invitations: {
+      type: Array,
+      required: true
+    }
+  },
+  setup(props) {
+    const { loadProjectModels } = useModels();
+
+    let reloadDebounce = null;
+    const reloadModels = () => {
+      clearTimeout(reloadDebounce);
+      reloadDebounce = setTimeout(async () => {
+        await loadProjectModels(props.project);
+      }, 1000);
+    };
+
+    const showFileUploader = ref(false);
+    const openFileUploader = () => {
+      showFileUploader.value = true;
+    };
+    const closeFileUploader = () => {
+      showFileUploader.value = false;
+    };
+    const toggleFileUploader = () => {
+      showFileUploader.value = !showFileUploader.value;
+    };
+
+    return {
+      // References
+      showFileUploader,
+      // Methods
+      closeFileUploader,
+      openFileUploader,
+      reloadModels,
+      toggleFileUploader
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss" src="./ProjectOverview.scss"></style>
