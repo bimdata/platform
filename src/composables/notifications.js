b4e05612b845d20f8b5f7031639ccc3815146de8
NicolasRichel <nicolas@bimdata.io>
2021-06-10 11:33:16 +0200

    feat(ifc-upload): notify user when trying to upload a non ifc file

--- src/composables/notifications.js
+++ src/composables/notifications.js
@@ -1,15 +1,23 @@
 import { ref } from "vue";
 import { delay } from "@/utils/async";
 
-const showNotification = ref(false);
-const notification = ref({});
+const DEFAULT_TIMEOUT = 5000;
+const EMPTY_NOTIFICATION = {
+  type: "info",
+  title: "",
+  message: ""
+};
 
-const pushNotification = async (notif, timeout = 5000) => {
+const showNotification = ref(false);
+const notification = ref(EMPTY_NOTIFICATION);
+
+const pushNotification = async (notif, timeout = DEFAULT_TIMEOUT) => {
   notification.value = notif;
   showNotification.value = true;
   await delay(timeout);
   showNotification.value = false;
-  notification.value = {};
+  await delay(100);
+  notification.value = EMPTY_NOTIFICATION;
 };
 
 export function useNotifications() {
