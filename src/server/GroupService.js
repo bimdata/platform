8be04f2ede4f363520b9cecee39268003f5246d1
NicolasRichel <nicolas@bimdata.io>
2021-06-25 10:44:19 +0200

    fix: disable groups test data

--- src/server/GroupService.js
+++ src/server/GroupService.js
@@ -3,44 +3,44 @@ import apiClient from "./api-client";
 class GroupService {
   fetchProjectGroups(project) {
     return [
-      {
-        id: 1,
-        name: "My Group Name",
-        color: "#FF3D1E",
-        project,
-        members: [
-          {
-            id: 1230,
-            firstname: "Gaëlle",
-            lastname: "Leroux",
-            email: "gaelle@bimdata.io"
-          },
-          {
-            id: 1231,
-            firstname: "Gaëtan",
-            lastname: "Lagier",
-            email: "gaetan@bimdata.io"
-          },
-          {
-            id: 1232,
-            firstname: "Hugo",
-            lastname: "Duroux",
-            email: "hugo@bimdata.io"
-          },
-          {
-            id: 1234,
-            firstname: "Nicolas",
-            lastname: "Richel",
-            email: "nicolas@bimdata.io"
-          },
-          {
-            id: 1235,
-            firstname: "François",
-            lastname: "Thierry",
-            email: "francois@bimdata.io"
-          }
-        ]
-      }
+      // {
+      //   id: 1,
+      //   name: "My Group Name",
+      //   color: "#FF3D1E",
+      //   project,
+      //   members: [
+      //     {
+      //       id: 1230,
+      //       firstname: "Gaëlle",
+      //       lastname: "Leroux",
+      //       email: "gaelle@bimdata.io"
+      //     },
+      //     {
+      //       id: 1231,
+      //       firstname: "Gaëtan",
+      //       lastname: "Lagier",
+      //       email: "gaetan@bimdata.io"
+      //     },
+      //     {
+      //       id: 1232,
+      //       firstname: "Hugo",
+      //       lastname: "Duroux",
+      //       email: "hugo@bimdata.io"
+      //     },
+      //     {
+      //       id: 1234,
+      //       firstname: "Nicolas",
+      //       lastname: "Richel",
+      //       email: "nicolas@bimdata.io"
+      //     },
+      //     {
+      //       id: 1235,
+      //       firstname: "François",
+      //       lastname: "Thierry",
+      //       email: "francois@bimdata.io"
+      //     }
+      //   ]
+      // }
     ];
   }
 
