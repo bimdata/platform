efdfe4172c1708b50e36bfa9d033e78ec46d0ae2
NicolasRichel <nicolas@bimdata.io>
2021-06-29 10:03:19 +0200

    test(e2e): rearrange mock definition & setup

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -3,6 +3,8 @@
 // https://docs.cypress.io/api/cypress-api/custom-commands
 // ***********************************************
 
+import { apiMocks, fileMocks } from "./mocks";
+
 Cypress.Commands.add("getHook", names => {
   const selector = names.split(".").map(name => `[data-test=${name}]`).join(" ");
   // eslint-disable-next-line cypress/require-data-selectors
@@ -29,36 +31,6 @@ Cypress.Commands.add("fileMock", ({ path, data }) => {
 });
 
 Cypress.Commands.add("setupMocks", () => {
-  const apiMocks = [
-    { name: "user",                    path: "/user",                             data: "user.json" },
-    { name: "spaces",                  path: "/cloud",                            data: "spaces.json" },
-    { name: "projects",                path: "/user/projects",                    data: "projects.json" },
-    { name: "space-100-user",          path: "/cloud/100/user",                   data: "spaces/100/users.json" },
-    { name: "space-100-invitation",    path: "/cloud/100/invitation",             data: "spaces/100/invitations.json" },
-    { name: "space-200-user",          path: "/cloud/200/user",                   data: "spaces/200/users.json" },
-    { name: "space-200-invitation",    path: "/cloud/200/invitation",             data: "spaces/200/invitations.json" },
-    { name: "space-300-user",          path: "/cloud/300/user",                   data: "spaces/300/users.json" },
-    { name: "space-300-invitation",    path: "/cloud/300/invitation",             data: "spaces/300/invitations.json" },
-    { name: "project-100-users",       path: "/cloud/100/project/100/user",       data: "projects/100/users.json" },
-    { name: "project-100-invitations", path: "/cloud/100/project/100/invitation", data: "projects/100/invitations.json" },
-    { name: "project-100-models",      path: "/cloud/100/project/100/ifc",        data: "projects/100/models.json" },
-    { name: "project-101-users",       path: "/cloud/100/project/101/user",       data: "projects/101/users.json" },
-    { name: "project-101-invitations", path: "/cloud/100/project/101/invitation", data: "projects/101/invitations.json" },
-    { name: "project-101-models",      path: "/cloud/100/project/101/ifc",        data: "projects/101/models.json" },
-    { name: "project-200-users",       path: "/cloud/200/project/200/user",       data: "projects/200/users.json" },
-    { name: "project-200-invitations", path: "/cloud/200/project/200/invitation", data: "projects/200/invitations.json" },
-    { name: "project-200-models",      path: "/cloud/200/project/200/ifc",        data: "projects/200/models.json" },
-    { name: "project-300-users",       path: "/cloud/300/project/300/user",       data: "projects/300/users.json" },
-    { name: "project-300-invitations", path: "/cloud/300/project/300/invitation", data: "projects/300/invitations.json" },
-    { name: "project-300-models",      path: "/cloud/300/project/300/ifc",        data: "projects/300/models.json" },
-    { name: "model-1001-site",         path: "/cloud/100/project/100/ifc/1001/element?type=IfcSite", data: "models/1001/ifc-site.json" },
-    { name: "model-1002-site",         path: "/cloud/100/project/100/ifc/1002/element?type=IfcSite", data: "models/1002/ifc-site.json" }
-  ];
-  const fileMocks = [
-    { path: "/space-image/100",    data: "spaces/100/image.jpg" },
-    { path: "/model-preview/1001", data: "models/1001/preview.png" },
-    { path: "/model-preview/1002", data: "models/1002/preview.png" }
-  ];
   apiMocks.forEach(cy.apiMock);
   fileMocks.forEach(cy.fileMock);
 });
---
+++ tests/e2e/support/mocks.js
@@ -0,0 +1,88 @@
+function createMock(spaceID, projectID, modelIDs = []) {
+  const mock = [
+    {
+      name: `space-${spaceID}-user`,
+      path: `/cloud/${spaceID}/user`,
+      data: `spaces/${spaceID}/users.json`
+    },
+    {
+      name: `space-${spaceID}-invitation`,
+      path: `/cloud/${spaceID}/invitation`,
+      data: `spaces/${spaceID}/invitations.json`
+    },
+    {
+      name: `project-${projectID}-users`,
+      path: `/cloud/${spaceID}/project/${projectID}/user`,
+      data: `projects/${projectID}/users.json`
+    },
+    {
+      name: `project-${projectID}-invitations`,
+      path: `/cloud/${spaceID}/project/${projectID}/invitation`,
+      data: `projects/${projectID}/invitations.json`
+    },
+    {
+      name: `project-${projectID}-models`,
+      path: `/cloud/${spaceID}/project/${projectID}/ifc`,
+      data: `projects/${projectID}/models.json`
+    },
+    {
+      name: `project-${projectID}-files`,
+      path: `/cloud/${spaceID}/project/${projectID}/dms-tree`,
+      data: `projects/${projectID}/file-structure.json`
+    },
+    {
+      name: `project-${projectID}-groups`,
+      path: `/cloud/${spaceID}/project/${projectID}/group`,
+      data: `projects/${projectID}/groups.json`
+    }
+  ];
+  for (const modelID of modelIDs) {
+    mock.concat([
+      {
+        name: `model-${modelID}-site`,
+        path: `/cloud/${spaceID}/project/${projectID}/ifc/${modelID}/element?type=IfcSite`,
+        data: `models/${modelID}/ifc-site.json`
+      },
+      { name: `model-${modelID}-site`,
+        path: `/cloud/${spaceID}/project/${projectID}/ifc/${modelID}/element?type=IfcSite`,
+        data: `models/${modelID}/ifc-site.json`
+      }
+    ]);
+  }
+  return mock;
+}
+
+function spaceImage(spaceID) {
+  return {
+    path: `/space-image/${spaceID}`,
+    data: `spaces/${spaceID}/image.jpg`
+  };
+}
+
+function modelPreview(modelID) {
+  return {
+    path: `/model-preview/${modelID}`,
+    data: `models/${modelID}/preview.png`
+  };
+}
+
+const apiMocks = [
+  { name: "user",     path: "/user",          data: "user.json"     },
+  { name: "spaces",   path: "/cloud",         data: "spaces.json"   },
+  { name: "projects", path: "/user/projects", data: "projects.json" },
+  ...createMock(100, 100, [1001, 1002]),
+  ...createMock(100, 101, []),
+  ...createMock(200, 200, []),
+  ...createMock(300, 300, [])
+];
+
+const fileMocks = [
+  spaceImage(100),
+  modelPreview(1001),
+  modelPreview(1002)
+];
+
+export {
+  apiMocks,
+  fileMocks
+};
