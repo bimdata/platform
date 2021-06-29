f174fa8bb2e139b71906f5b94aeb45cc9677a645
amoki <hugo@bimdata.io>
2021-06-29 16:06:14 +0200

    WIP: first Paddle implementation

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -75,7 +75,6 @@ export default {
     };
 
     const buyPlatformPro = () => {
-
       Paddle.Product.Prices(12403, function (prices) {
         // TODO: set price with with function instead of hard coded value
         console.log(prices);
