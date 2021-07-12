26f44fb6c939f6b432c65b5bc3e0cbcf88b86ac9
NicolasRichel <nicolas@bimdata.io>
2021-07-12 10:46:37 +0200

    chore: lint @/utils/auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
 function parseJwt(token) {
   try {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace("-", "+").replace("_", "/");
     return JSON.parse(window.atob(base64));
   } catch (error) {
     return {};
