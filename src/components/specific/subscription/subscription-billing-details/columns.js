d5f6369ac0ff14a7aef9bad927ab11a9868e6675
Gaelle <gaelle@bimdata.io>
2021-09-21 09:33:44 +0200

    add BillingDetails specific component

---
+++ src/components/specific/subscription/subscription-billing-details/columns.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: "space"
+  },
+  {
+    id: "nextpayment",
+  },
+  {
+    id: "subscriptionplan",
+  },
+  {
+    id: "statut",
+  },
+  {
+    id: "amount",
+  },
+  {
+    id: "actions",
+    label: " ",
+    width: "50px",
+    align: "center"
+  }
+];
