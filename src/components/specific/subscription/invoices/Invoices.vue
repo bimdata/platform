6dc5167fb183fae653c1272fe2c60943cdab4c12
Gaelle <gaelle@bimdata.io>
2021-09-21 09:33:25 +0200

    add Invoices specific component

---
+++ src/components/specific/subscription/invoices/Invoices.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="platform-subscription__content__invoices">
+    <h2>Invoices</h2>
+    <BIMDataCard>
+      <template #content>
+        <GenericTable
+          class="billing-table"
+          :columns="columns"
+          :rows="invoices"
+          :paginated="true"
+          :perPage="7"
+          :placeholder="$t('Invoices.emptyTablePlaceholder')"
+        >
+          <template #cell-version="{ row: { version } }">
+            {{ version ? version : "-" }}
+          </template>
+        </GenericTable>
+      </template>
+    </BIMDataCard>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+import { useI18n } from "vue-i18n";
+import columnsDef from "./columns";
+// Components
+import GenericTable from "@/components/generic/generic-table/GenericTable";
+export default {
+  components: {
+    GenericTable,
+  },
+  props: {
+    invoices: {
+      type: Array,
+      required: true,
+    },
+    empty: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {
+    const { locale, t } = useI18n();
+    const columns = ref([]);
+    watch(
+      () => locale.value,
+      () => {
+        columns.value = columnsDef.map((col) => ({
+          ...col,
+          label: col.label || t(`Invoices.headers.${col.id}`),
+        }));
+      },
+      { immediate: true }
+    );
+    return {
+      columns,
+    };
+  },
+};
+</script>
---
+++ src/components/specific/subscription/invoices/columns.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: "amount"
+  },
+  {
+    id: "statut",
+  },
+  {
+    id: "space",
+  },
+  {
+    id: "date",
+  },
+  {
+    id: "actions",
+    width: "50px",
+  }
+];
