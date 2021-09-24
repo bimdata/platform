bcee875a8fc7b367008e423ca280e97b622c0d5f
NicolasRichel <nicolas@bimdata.io>
2021-09-24 14:45:15 +0200

    feat(state): little reword + load user organizations in root resolver

--- src/router/resolvers/root.js
+++ src/router/resolvers/root.js
@@ -1,16 +1,20 @@
 import { useProjects } from "@/state/projects.js";
 import { useSpaces } from "@/state/spaces.js";
 import { useUser } from "@/state/user.js";
+import { useOrganizations } from "@/state/organizations.js";
 
 let rootResolved = false;
 
 const rootResolver = async () => {
   const { loadUser } = useUser();
+  const { retrieveUserOrganizations: loadUserOrganizations } =
+    useOrganizations();
   const { loadUserSpaces } = useSpaces();
   const { loadUserProjects } = useProjects();
 
   if (!rootResolved) {
     await loadUser();
+    await loadUserOrganizations();
     await loadUserSpaces();
     await loadUserProjects();
     rootResolved = true;
