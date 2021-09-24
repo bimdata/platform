73d79dba3ae09b2ad1f01c15f172b1fb0933d2ec
NicolasRichel <nicolas@bimdata.io>
2021-09-24 09:02:58 +0200

    feat(state): create organization service/state

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -4,4 +4,35 @@ const apiClient = makeBIMDataApiClient({
   apiUrl: process.env.VUE_APP_API_BASE_URL
 });
 
+const privateApiFetch = async ({ method = "GET", path, body }) => {
+  const response = await fetch(
+    `${process.env.VUE_APP_API_BASE_URL}/private${path}`,
+    {
+      method,
+      headers: {
+        Authorization: apiClient.config.accessToken()
+      },
+      body: body ? JSON.stringify(body) : undefined
+    }
+  );
+  return await response.json();
+};
+
+const privateApiClient = {
+  get(path) {
+    return privateApiFetch({ path });
+  },
+  post(path, body) {
+    return privateApiFetch({ method: "POST", path, body });
+  },
+  patch(path, body) {
+    return privateApiFetch({ method: "PATCH", path, body });
+  },
+  delete(path) {
+    return privateApiFetch({ method: "DELETE", path });
+  }
+};
+
 export default apiClient;
+
+export { privateApiClient };
