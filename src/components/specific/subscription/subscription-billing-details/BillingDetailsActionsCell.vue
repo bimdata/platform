462118790bc470188805f6b83f3bca593c539d9c
Gaelle <gaelle@bimdata.io>
2021-10-02 10:41:23 +0200

    custom organization Billing details

---
+++ src/components/specific/subscription/subscription-billing-details/BillingDetailsActionsCell.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="billing-actions-cell" v-click-away="closeMenu">
+    <BIMDataButton
+      class="billing-actions-cell__btn"
+      ripple
+      rounded
+      icon
+      @click="toggleMenu"
+    >
+      <BIMDataIcon name="ellipsis" size="l" />
+    </BIMDataButton>
+
+    <transition name="fade">
+      <div class="billing-actions-cell__menu" v-show="showMenu">
+        <a
+          :href="billing.update_url"
+          class="
+            bimdata-btn bimdata-btn__ghost bimdata-btn__ghost--default
+            billing-actions-cell__menu__btn
+          "
+        >
+          Modifier la carte de paiement</a
+        >
+        <a
+          :href="billing.cancel_url"
+          class="
+            bimdata-btn bimdata-btn__ghost bimdata-btn__ghost--high
+            billing-actions-cell__menu__btn
+          "
+        >
+          Annuler ce paiement</a
+        >
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { useToggle } from "@/composables/toggle";
+
+export default {
+  props: {
+    billing: {
+      type: Object,
+      required: true
+    }
+  },
+  setup() {
+    const {
+      isOpen: showMenu,
+      close: closeMenu,
+      toggle: toggleMenu
+    } = useToggle();
+
+    return {
+      // References
+      showMenu,
+      // Methods
+      closeMenu,
+      toggleMenu
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss" src="./BillingDetailsActionsCell.scss"></style>
