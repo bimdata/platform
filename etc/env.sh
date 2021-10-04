4d37d384bcf6c6cd7f308e39fbcf1f3a6b238d1c
RomainMou <58464216+RomainMou@users.noreply.github.com>
2021-10-04 15:43:31 +0200

    Improve env script to be able to support unset variables.

--- etc/env.sh
+++ etc/env.sh
@@ -5,18 +5,31 @@ set -euo pipefail
 js_original_path="/usr/share/nginx/html/js-original"
 js_path="/usr/share/nginx/html/js"
 
+# Associative var_name => placeholder_value
+declare -A vars_to_placeholders=(
+  [VUE_APP_BASE_URL]="ENV_TOKEN_BASE_URL"
+  [VUE_APP_IAM_BASE_URL]="ENV_TOKEN_IAM_BASE_URL"
+  [VUE_APP_API_BASE_URL]="ENV_TOKEN_API_BASE_URL"
+  [VUE_APP_BACKEND_BASE_URL]="ENV_TOKEN_BACKEND_BASE_URL"
+  [VUE_APP_OIDC_CLIENT_ID]="ENV_TOKEN_OIDC_CLIENT_ID"
+  [VUE_APP_MAPBOX_TOKEN]="ENV_TOKEN_MAPBOX_TOKEN"
+  [VUE_APP_MAX_UPLOAD_SIZE]="ENV_TOKEN_MAX_UPLOAD_SIZE"
+  [VUE_APP_URL_BIMDATACONNECT]="ENV_TOKEN_URL_BIMDATACONNECT"
+  [VUE_APP_URL_DOCUMENTATION]="ENV_TOKEN_URL_DOCUMENTATION"
+  [VUE_APP_URL_MARKETPLACE]="ENV_TOKEN_URL_MARKETPLACE"
+  [VUE_APP_ARCHIVE_BASE_URL]="ENV_TOKEN_ARCHIVE_BASE_URL"
+  [VUE_APP_URL_OLD_PLATFORM]="ENV_TOKEN_URL_OLD_PLATFORM"
+  [VUE_APP_AUTHORIZED_IDENTITY_PROVIDERS]="ENV_TOKEN_AUTHORIZED_IDENTITY_PROVIDERS"
+)
+
+# Copy the original JS
 cp -r ${js_original_path} ${js_path}
 
-sed -i "s|ENV_TOKEN_BASE_URL|$VUE_APP_BASE_URL|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_IAM_BASE_URL|$VUE_APP_IAM_BASE_URL|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_API_BASE_URL|$VUE_APP_API_BASE_URL|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_BACKEND_BASE_URL|$VUE_APP_BACKEND_BASE_URL|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_OIDC_CLIENT_ID|$VUE_APP_OIDC_CLIENT_ID|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_MAPBOX_TOKEN|$VUE_APP_MAPBOX_TOKEN|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_MAX_UPLOAD_SIZE|$VUE_APP_MAX_UPLOAD_SIZE|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_URL_BIMDATACONNECT|$VUE_APP_URL_BIMDATACONNECT|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_URL_DOCUMENTATION|$VUE_APP_URL_DOCUMENTATION|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_URL_MARKETPLACE|$VUE_APP_URL_MARKETPLACE|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_ARCHIVE_BASE_URL|$VUE_APP_ARCHIVE_BASE_URL|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_URL_OLD_PLATFORM|$VUE_APP_URL_OLD_PLATFORM|g" ${js_path}/*
-sed -i "s|ENV_TOKEN_AUTHORIZED_IDENTITY_PROVIDERS|$VUE_APP_AUTHORIZED_IDENTITY_PROVIDERS|g" ${js_path}/*
+# For each env variable, if unset, set it to empty string and replace
+# the placeholder by the wanted value in the JS files
+for var_name in "${!vars_to_placeholders[@]}" ; do
+  if [[ -z ${!var_name+x} ]] ; then
+    declare ${var_name}=""
+  fi
+  sed -i "s|${vars_to_placeholders[$var_name]}|${!var_name}|g" "${js_path}/*"
+done
