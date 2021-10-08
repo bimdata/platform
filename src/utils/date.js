9f3c8e3fa408d675b2abb528fe9089f7d456590c
NicolasRichel <nicolas@bimdata.io>
2021-10-08 11:29:57 +0200

    chore: some fixes after rebase

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function formatDate(value) {
   const monthNames = [
     "January",
@@ -22,17 +21,17 @@ function formatDate(value) {
     " " +
     date.getFullYear()
   );
-=======
+}
+
 /**
  * Change an ISO formatted date into a date with formatted as: DD/MM/YYYY.
  *
  * @param {String} date an ISO formatted date string
  * @returns {String} formatted date
  */
-function formatDate(date) {
+function formatDateShort(date) {
   const d = date.split("T")[0];
   return d.split("-").reverse().join("/");
->>>>>>> 8f347fc (feat(organizations): create organization management panel on user spaces view)
 }
 
-export { formatDate };
+export { formatDate, formatDateShort };
