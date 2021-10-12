7921dd692cd303fec6c1a0eb16bbb0a6a44194e7
NicolasRichel <nicolas@bimdata.io>
2021-10-12 14:09:48 +0200

    feat(orga): refactor component structure for OrganizationsManager

---
+++ src/components/specific/organizations/organizations-manager/organization-delete-guard/OrganizationDeleteGuard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="organization-delete-guard">
+    <!-- TODO -->
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    // TODO
+  }
+};
+</script>
+
+<style scoped lang="scss" src="./OrganizationDeleteGuard.scss"></style>
--- src/components/specific/organizations/organizations-manager/organization-form/OrganizationForm.vue
+++ src/components/specific/organizations/organizations-manager/organization-form/OrganizationForm.vue
@@ -1,50 +1,14 @@
 <template>
   <div class="organization-form">
-    <div class="organization-form__header">
-      <BIMDataButton ghost radius :disabled="loading" @click="close">
-        <BIMDataIcon name="arrow" size="xxs" margin="0 6px 0 0" />
-        <span>{{ $t("OrganizationForm.backButtonText") }}</span>
-      </BIMDataButton>
-      <BIMDataButton ghost rounded icon :disabled="loading" @click="closePanel">
-        <BIMDataIcon name="close" size="xxs" fill color="tertiary-dark" />
-      </BIMDataButton>
-    </div>
-
     <transition name="fade" mode="out-in">
-      <template v-if="loading">
-        <div class="organization-form__loader">
-          <BIMDataSpinner />
-        </div>
-      </template>
-
-      <template v-else-if="isSuccess">
-        <div class="organization-form__success">
-          <img
-            class="organization-form__success__image"
-            src="/static/organization-form-success.svg"
-          />
-          <div class="organization-form__success__title">
-            {{ $t("OrganizationForm.successTitle") }}
-          </div>
-          <div class="organization-form__success__message">
-            {{ $t("OrganizationForm.successMessage") }}
-          </div>
-          <BIMDataButton
-            width="120px"
-            color="primary"
-            fill
-            radius
-            @click="reset"
-          >
-            {{ $t("OrganizationForm.successButtonText") }}
-          </BIMDataButton>
-        </div>
+      <template v-if="isSuccess">
+        <OrganizationFormSuccess @close="reset" />
       </template>
 
       <template v-else>
         <div class="organization-form__content">
           <div class="organization-form__title">
-            {{ "Création d'organisation" }}
+            {{ $t("OrganizationForm.title") }}
           </div>
           <div class="organization-form__section">
             <div class="organization-form__section__title">
@@ -79,46 +43,37 @@
 </template>
 
 <script>
-import { ref, watch } from "vue";
+import { inject, ref, watch } from "vue";
 import { useOrganizations } from "@/state/organizations.js";
+// Components
+import OrganizationFormSuccess from "./organization-form-success/OrganizationFormSuccess.vue";
 
 export default {
-  props: {
-    organization: {
-      type: Object,
-      default: null
-    }
+  components: {
+    OrganizationFormSuccess
   },
-  emits: ["close", "close-panel"],
-  setup(props, { emit }) {
+  setup() {
     const { createOrganization, updateOrganization } = useOrganizations();
 
-    const loading = ref(false);
+    const localState = inject("localState");
+
     const hasInvalidName = ref(false);
-    const isSuccess = ref(false);
+    const isSuccess = ref(true);
 
     const reset = () => {
-      loading.value = false;
+      localState.loading = false;
       hasInvalidName.value = false;
       isSuccess.value = false;
-    };
-    const close = () => {
-      reset();
-      emit("close");
-    };
-    const closePanel = () => {
-      reset();
-      emit("close-panel");
     };
 
     const orgaName = ref("");
     const orgaLogo = ref(null);
 
     watch(
-      () => props.organization,
+      () => localState.organization,
       () => {
-        if (props.organization) {
-          orgaName.value = props.organization.name;
+        if (localState.organization) {
+          orgaName.value = localState.organization.name;
         }
       },
       { immediate: true }
@@ -131,16 +86,16 @@ export default {
         return;
       }
       try {
-        loading.value = true;
+        localState.loading = true;
         const newOrganization = { name: orgaName.value };
-        if (props.organization) {
+        if (localState.organization) {
           await updateOrganization(newOrganization);
         } else {
           await createOrganization(newOrganization);
         }
         isSuccess.value = true;
       } finally {
-        loading.value = false;
+        localState.loading = false;
       }
     };
 
@@ -148,12 +103,9 @@ export default {
       // References
       hasInvalidName,
       isSuccess,
-      loading,
       orgaLogo,
       orgaName,
       // Methods
-      close,
-      closePanel,
       reset,
       submit
     };
