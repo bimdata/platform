e61581226e53a8c3744aa23f82825cf817c8cd4b
Gaelle <gaelle@bimdata.io>
2021-10-14 18:58:44 +0200

    use StorageInformations component

--- src/views/space-board/SpaceBoard.vue
+++ src/views/space-board/SpaceBoard.vue
@@ -16,38 +16,7 @@
       </template>
       <template #right>
         <div class="flex items-center">
-          <div class="flex" v-if="size.role === 100">
-            <ProgressBar class="m-r-12" :progressPercent="calcSize">
-              <template #text-left-below
-                >{{ $t("SpaceBoard.progressBar.using") }}
-                {{ formatBytes(size.smartDataSize) }}
-                {{ $t("SpaceBoard.progressBar.from") }}
-                {{ formatBytes(size.smartDataSizeAvailable) }}</template
-              >
-            </ProgressBar>
-            <div v-if="isPlatformSubscription && isOrganizationMember">
-              <BIMDataButton
-                v-if="isPlatformPaid"
-                color="secondary"
-                fill
-                radius
-                @click="goToPaddlePayment"
-                class="m-r-18"
-              >
-                {{ $t("SpaceBoard.progressBar.buyPlatformProButton") }}
-              </BIMDataButton>
-              <BIMDataButton
-                v-else
-                color="secondary"
-                fill
-                radius
-                @click="goToPaddlePayment"
-                class="m-r-18"
-              >
-                {{ $t("SpaceBoard.progressBar.upgradeStorageButton") }}
-              </BIMDataButton>
-            </div>
-          </div>
+          <StorageInformations :space="space" />
           <!-- <BIMDataButton
           data-test="btn-filter"
           class="space-board__header__btn-filter"
@@ -107,21 +76,13 @@
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
-
-import { useRouter } from "vue-router";
-import { routeNames } from "@/router";
-
 import { useListFilter } from "@/composables/list-filter";
 import { useListSort } from "@/composables/list-sort";
 import { useSidePanel } from "@/composables/side-panel";
-import { usePayment } from "@/state/payment.js";
 import { useProjects } from "@/state/projects";
 import { useSpaces } from "@/state/spaces";
 
-import { formatBytes } from "@/utils/formatBytes.js";
 // Components
-import ProgressBar from "@/components/generic/progress-bar/ProgressBar";
 import ResponsiveGrid from "@/components/generic/responsive-grid/ResponsiveGrid";
 import SidePanel from "@/components/generic/side-panel/SidePanel";
 import ViewHeader from "@/components/generic/view-header/ViewHeader";
@@ -129,25 +90,23 @@ import AppBreadcrumb from "@/components/specific/app/app-breadcrumb/AppBreadcrum
 import ProjectCard from "@/components/specific/projects/project-card/ProjectCard";
 import ProjectCreationCard from "@/components/specific/projects/project-creation-card/ProjectCreationCard";
 import SpaceUsersManager from "@/components/specific/users/space-users-manager/SpaceUsersManager";
+import StorageInformations from "@/components/specific/subscription/storage-informations/StorageInformations";
 
 export default {
   components: {
-    ProgressBar,
     ResponsiveGrid,
     SidePanel,
     ViewHeader,
     AppBreadcrumb,
     ProjectCard,
     ProjectCreationCard,
-    SpaceUsersManager
+    SpaceUsersManager,
+    StorageInformations
   },
   setup() {
-    const { currentSpace, spaceUsers, spaceInvitations, spaceSize } =
-      useSpaces();
+    const { currentSpace, spaceUsers, spaceInvitations } = useSpaces();
     const { spaceProjects } = useProjects();
     const { openSidePanel } = useSidePanel();
-    const { retrievePlaformSubscriptions } = usePayment();
-    const router = useRouter();
 
     const { filteredList: displayedProjects, searchText } = useListFilter(
       spaceProjects,
@@ -159,44 +118,6 @@ export default {
       project => project.name
     );
 
-    const size = ref({});
-    const currentOrganization = currentSpace.value.organization;
-    const isPlatformSubscription = ref(false);
-    const isOrganizationMember = ref(false);
-    const isPlatformPaid = ref(false);
-    const calcSize = ref(Number);
-
-    onMounted(async () => {
-      // cloud size value
-      size.value = await spaceSize(currentSpace.value);
-
-      isPlatformSubscription.value =
-        size.value.managedBy === "BIMDATA_PLATFORM";
-
-      if (isPlatformSubscription.value) {
-        try {
-          let platformSubscriptions = await retrievePlaformSubscriptions(
-            currentOrganization,
-            currentSpace.value
-          );
-          isOrganizationMember.value = true;
-          // boolean for upgrade platform or pay platform pro
-          isPlatformPaid.value = platformSubscriptions.some(
-            platformSubscription => platformSubscription.status === "active"
-          );
-        } catch (e) {
-          isOrganizationMember.value = false;
-        }
-      }
-
-      // calculation of the remaining size
-      calcSize.value = 100 - size.value.remainingSmartDataSizePercent;
-    });
-
-    const goToPaddlePayment = () => {
-      router.push({ name: routeNames.paddlePayment });
-    };
-
     return {
       // References
       invitations: spaceInvitations,
@@ -204,17 +125,9 @@ export default {
       searchText,
       space: currentSpace,
       users: spaceUsers,
-      isPlatformSubscription,
-      isPlatformPaid,
-      isOrganizationMember,
-      size,
-      formatBytes,
-      calcSize,
-      currentOrganization,
       // Methods
       openUsersManager: openSidePanel,
-      sortProjects,
-      goToPaddlePayment
+      sortProjects
     };
   }
 };
