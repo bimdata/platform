ac7849ac1bbe61e23e92a21ff1ffe264af934c01
NicolasRichel <nicolas@bimdata.io>
2021-10-21 13:44:37 +0200

    refactor: create PlatformSubscription sub view and use it in Payment view

---
+++ src/views/payment/platform-subscription/PlatformSubscription.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="platform-subscription">
+    <div class="platform-subscription__left">
+      <PlatformSubInfo />
+    </div>
+    <div class="platform-subscription__center">
+      <PlatformSubForm :space="space" />
+    </div>
+    <div class="platform-subscription__right">
+      <SpaceSizePreview :space="space" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+// Components
+import PlatformSubForm from "@/components/specific/payment/platform-sub-form/PlatformSubForm.vue";
+import PlatformSubInfo from "@/components/specific/payment/platform-sub-info/PlatformSubInfo.vue";
+import SpaceSizePreview from "@/components/specific/payment/space-size-preview/SpaceSizePreview.vue";
+
+export default {
+  components: {
+    PlatformSubForm,
+    PlatformSubInfo,
+    SpaceSizePreview
+  },
+  setup() {
+    const space = ref({});
+
+    return {
+      // References
+      space
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss" src="./PlatformSubscription.scss"></style>
