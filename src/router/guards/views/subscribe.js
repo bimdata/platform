de7737b8cd9a8e3e76423a50b7719e2c67c8965e
NicolasRichel <nicolas@bimdata.io>
2021-11-16 18:20:44 +0100

    fix(routing): redirect free plan to pro plan if user have already a free space

--- src/router/guards/views/subscribe.js
+++ src/router/guards/views/subscribe.js
@@ -3,11 +3,9 @@ import SubscriptionService from "@/services/SubscriptionService.js";
 
 export default async function subscribeGuard() {
   // Redirect to pro plan subscription
-  // if the user already has at least one free space
+  // if the user already has a free space
   const freeSpaces = await SubscriptionService.fetchFreeSpaces();
   if (freeSpaces.length > 0) {
     return { name: routeNames.subscriptionPro };
   }
-
-  return true;
 }
--- src/router/guards/views/subscription-free.js
+++ src/router/guards/views/subscription-free.js
@@ -1,8 +1,17 @@
+import routeNames from "@/router/route-names.js";
+import SubscriptionService from "@/services/SubscriptionService.js";
 import { useSubscriptions } from "@/state/subscriptions.js";
 
 const { setCurrentOrga } = useSubscriptions();
 
 export default async function subscriptionFreeGuard(route) {
+  // Redirect to pro plan subscription
+  // if the user already has a free space
+  const freeSpaces = await SubscriptionService.fetchFreeSpaces();
+  if (freeSpaces.length > 0) {
+    return { name: routeNames.subscriptionPro };
+  }
+
   setCurrentOrga(null);
 
   if (route.query.organization) {
