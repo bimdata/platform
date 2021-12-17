c2bd726bba32b90ff81ad36b93a294badd52575d
NicolasRichel <nicolas@bimdata.io>
2021-12-17 14:11:19 +0100

    feat(models-manager): add models-manager menu

--- src/components/specific/models/models-manager/ModelsManager.vue
+++ src/components/specific/models/models-manager/ModelsManager.vue
@@ -1,34 +1,55 @@
 <template>
   <div class="models-manager">
     <div class="models-manager__head">
-      <div
-        v-for="tab of tabs"
-        :key="tab.id"
-        class="models-manager__tab"
-        :class="{ selected: tab.id === currentTab.id }"
-        @click="selectTab(tab)"
-      >
-        <span class="models-manager__tab__icon">
-          <img :src="`/static/${tab.id}-file.svg`" />
-        </span>
-        <span class="models-manager__tab__text">
-          {{ tab.label }}
-        </span>
-        <span class="models-manager__tab__count" v-if="tab.models.length > 0">
-          {{ tab.models.length }}
-        </span>
-      </div>
+      <template v-for="tab of tabs" :key="tab.id">
+        <div
+          v-if="tab.displayed"
+          class="models-manager__tab"
+          :class="{ selected: tab.id === currentTab.id }"
+          @click="selectTab(tab)"
+        >
+          <span class="models-manager__tab__icon">
+            <img :src="`/static/${tab.id}-file.svg`" />
+          </span>
+          <span class="models-manager__tab__text">
+            {{ tab.label }}
+          </span>
+          <span class="models-manager__tab__count" v-if="tab.models.length > 0">
+            {{ tab.models.length }}
+          </span>
+        </div>
+      </template>
 
-      <BIMDataButton>
-        <BIMDataIcon
-          name="ellipsis"
-          size="l"
-          fill
-          color="granite-light"
-          :rotate="90"
-        />
-      </BIMDataButton>
+      <div class="models-manager__menu">
+        <BIMDataButton>
+          <BIMDataIcon
+            color="granite-light"
+            name="ellipsis"
+            size="l"
+            fill
+            :rotate="90"
+            @click="toggleMenu"
+          />
+        </BIMDataButton>
+
+        <div class="models-manager__menu__container" v-show="showMenu">
+          <div
+            class="models-manager__menu__item"
+            :class="{ disabled: tab.models.length > 0 }"
+            v-for="tab of tabs"
+            :key="tab.id"
+          >
+            <BIMDataCheckbox
+              :disabled="tab.models.length > 0"
+              :modelValue="tab.models.length > 0 || tab.displayed"
+              @update:modelValue="tab.displayed = !tab.displayed"
+            />
+            <span>{{ `.${tab.id}` }}</span>
+          </div>
+        </div>
+      </div>
     </div>
+
     <div class="models-manager__body">
       <transition name="fade">
         <keep-alive>
@@ -45,6 +66,7 @@
 
 <script>
 import { ref, watch } from "vue";
+import { useToggle } from "@/composables/toggle.js";
 // Components
 import DWGManager from "./dwg-manager/DWGManager.vue";
 import IFCManager from "./ifc-manager/IFCManager.vue";
@@ -76,6 +98,8 @@ export default {
     }
   },
   setup(props) {
+    const { isOpen: showMenu, toggle: toggleMenu } = useToggle();
+
     const tabs = ref([]);
     const currentTab = ref({});
 
@@ -89,7 +113,8 @@ export default {
         tabs.value = tabsDef.map(tab => ({
           ...tab,
           // TODO: set models for each tab based on type
-          models: tab.id === "ifc" ? models : []
+          models: tab.id === "ifc" ? models : [],
+          displayed: true
         }));
         currentTab.value = tabs.value[0];
       },
@@ -99,9 +124,11 @@ export default {
     return {
       // References
       currentTab,
+      showMenu,
       tabs,
       // Methods
-      selectTab
+      selectTab,
+      toggleMenu
     };
   }
 };
