27ff903b76f19b7db173f5fb2df515b03a04f581
NicolasRichel <nicolas@bimdata.io>
2022-01-04 15:18:56 +0100

    feat(payment): put paddle config into env

--- src/composables/paddle.js
+++ src/composables/paddle.js
@@ -1,5 +1,19 @@
-import { DATAPACK_PLAN_ID, PRO_PLAN_ID } from "@/config/subscription.js";
+/* eslint no-undef: "off" */
+
+import {
+  DATAPACK_PLAN_ID,
+  IS_PADDLE_SANDBOX,
+  IS_SUBSCRIPTION_ENABLED,
+  PADDLE_VENDOR_ID,
+  PRO_PLAN_ID
+} from "@/config/subscription.js";
 import { getPrice } from "@/utils/price.js";
+
+// Setup Paddle configuration (on module load)
+if (IS_SUBSCRIPTION_ENABLED) {
+  if (IS_PADDLE_SANDBOX) Paddle.Environment.set("sandbox");
+  Paddle.Setup({ vendor: +PADDLE_VENDOR_ID });
+}
 
 const loadCheckout = (containerID, link, locale, { onLoad, onSuccess }) => {
   Paddle.Checkout.open({
