5e53e610a4c2daf55aa5aab91dc826556b7f31da
Gaelle <gaelle@bimdata.io>
2022-01-06 19:05:45 +0100

    add AppSlotContent & sort, filters buttons

--- src/views/project-board/project-bcf/ProjectBcf.vue
+++ src/views/project-board/project-bcf/ProjectBcf.vue
@@ -1,19 +1,56 @@
 <template>
-  <div class="project-bcf flex">
-    Some stats here
-    <BcfTopic :project="project" />
+  <div class="project-bcf">
+    <app-slot-content name="project-board-action">
+      <BIMDataButton width="140px" color="primary" fill radius>
+        <BIMDataIcon name="plus" size="xxxs" margin="0 6px 0 0" />
+        <span>Ajouter un BCF</span>
+      </BIMDataButton>
+    </app-slot-content>
+    <div class="flex justify-between m-t-24">
+      <BIMDataSearch placeholder="Search" color="secondary" radius width="35%">
+      </BIMDataSearch>
+      <div class="flex">
+        <BIMDataButton color="default" fill square icon class="m-r-12">
+          #
+        </BIMDataButton>
+        <BIMDataButton color="default" fill square icon class="m-r-12">
+          <BIMDataIcon name="alphabeticalSort" fill color="default" />
+        </BIMDataButton>
+        <BIMDataButton color="default" fill square icon class="m-r-12">
+          Date
+        </BIMDataButton>
+        <BIMDataButton color="default" fill square icon class="m-r-12">
+          List
+        </BIMDataButton>
+        <BIMDataButton color="default" fill square width="124px">
+          Filters
+        </BIMDataButton>
+      </div>
+    </div>
+    <div class="project-bcf__content flex m-t-36">
+      <div
+        class="project-bcf__content__metrics flex items-center justify-center"
+        style="width: 701px; background-color: white"
+      >
+        Some stats here
+      </div>
+      <BcfTopics :project="project" />
+    </div>
   </div>
 </template>
 
 <script>
 import { useProjects } from "@/state/projects.js";
 
+import AppSlotContent from "@/components/generic/app-slot/AppSlotContent.vue";
+
 // Components
-import BcfTopic from "./BcfTopic.vue";
+import BcfTopics from "../../../components/specific/bcf/bcf-topics/BcfTopics.vue";
 
 export default {
   components: {
-    BcfTopic
+    BcfTopics,
+    AppSlotContent
   },
   setup() {
     const { currentProject } = useProjects();
