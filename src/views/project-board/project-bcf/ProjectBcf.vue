269a75ea5d5745fd06a6c124a58f2818dda77549
Gaelle <gaelle@bimdata.io>
2022-01-07 09:23:51 +0100

    add bcfTopics to projectBcf view & custom project-bcf__content

--- src/views/project-board/project-bcf/ProjectBcf.vue
+++ src/views/project-board/project-bcf/ProjectBcf.vue
@@ -1,25 +1,48 @@
 <template>
-  <div class="project-bcf flex">
-    Some stats here
-    <BcfTopic :project="project" />
+  <div class="project-bcf__content flex m-t-36">
+    <div
+      class="project-bcf__content__metrics flex items-center justify-center"
+      style="width: 701px; background-color: white"
+    >
+      Some stats here
+    </div>
+    <BcfTopics
+      v-for="bcfTopic in bcfTopics"
+      :key="bcfTopic.index"
+      :project="project"
+      :bcfTopic="bcfTopic"
+    />
   </div>
 </template>
 
 <script>
+import { ref, watch } from "vue";
 import { useProjects } from "@/state/projects.js";
+import { useBcf } from "@/state/bcf.js";
 
 // Components
-import BcfTopic from "./BcfTopic.vue";
+import BcfTopics from "../../../components/specific/bcf/bcf-topics/BcfTopics.vue";
 
 export default {
   components: {
-    BcfTopic
+    BcfTopics
   },
   setup() {
     const { currentProject } = useProjects();
+    const { loadBcfTopics } = useBcf();
+    const bcfTopics = ref([]);
+
+    watch(
+      () => currentProject.value,
+      async () => {
+        bcfTopics.value = await loadBcfTopics(currentProject.value);
+      },
+      { immediate: true }
+    );
 
     return {
-      project: currentProject
+      project: currentProject,
+      bcfTopics
     };
   }
 };
