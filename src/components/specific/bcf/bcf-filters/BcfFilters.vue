d2f72dd63af2a33f680865b9a3e6e126ba41a7e0
LrxGaelle <gaelle@bimdata.io>
2022-01-13 18:23:01 +0100

    Feature/add sort and filters (#103)
    * export sortOrder to list-sort
    
    * add AlphabeticalAscending & AlphabeticalDescending icons
    
    * add sort by index, title & date
    
    * create BcfFilters component
    
    * use BcfFilters in ProjectBcf view
    
    * add priority & status lists filter
    
    * add search & loading to ProjectBcf
    
    * add components (elements) to bcfTopic object
    
    * add classes for priority and status for BcfTopics view
    
    * add BCF filters
    
    * fix filter.js - delete a character
    
    * use toggle composable for filters btn
    
    * add sort icons
    
    * sort bcfTopics by date by default
    
    * create list view for bcfTopics
    
    * create grid view for bcfTopics
    
    * use sort icons & add grid or list view
    
    * fix BcfPriorityCell view
    
    * add date & textBox title to BcfTopicsList
    
    * add btn actions to BcfTopicsList view
    
    * change width for actions in BcfTopicsList view
    
    * fix index & priority width

---
+++ src/components/specific/bcf/bcf-filters/BcfFilters.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="bcf-filters" v-click-away="closeFilters">
+    <BIMDataButton
+      data-test="btn-toggle-menu"
+      color="default"
+      fill
+      square
+      width="124px"
+      @click="toggleFilters"
+      class="btn-color-granite-light"
+      :class="{ 'btn-active': showFilters }"
+    >
+      Filters
+    </BIMDataButton>
+    <transition name="slide-fade-up">
+      <div class="bcf-filters__container p-18" v-show="showFilters">
+        <div
+          class="bcf-filters__container__header flex items-center justify-between"
+        >
+          <div class="bcf-filters__container__header__title">Filters</div>
+          <BIMDataButton color="primary" ghost rounded icon>
+            <BIMDataIcon
+              name="close"
+              size="xxs"
+              fill
+              color="primary"
+              @click="showFilters = false"
+            />
+          </BIMDataButton>
+        </div>
+        <BIMDataSelect
+          width="264px"
+          :multi="true"
+          label="Priority"
+          :options="priorityOptions"
+          v-model="priorities"
+          class="m-t-24"
+        />
+        <BIMDataSelect
+          width="264px"
+          :multi="true"
+          label="Status"
+          :options="statusOptions"
+          v-model="status"
+          class="m-t-24"
+        />
+        <!-- <div class="flex justify-between m-t-24">
+          <BIMDataSelect
+            width="47%"
+            label="Début de création"
+            :options="priorityOptions"
+            v-model="priority"
+          />
+          <BIMDataSelect
+            width="47%"
+            label="Fin de création"
+            :options="priorityOptions"
+            v-model="priority"
+          />
+        </div> -->
+        <BIMDataSelect
+          width="264px"
+          :multi="true"
+          label="Tags"
+          :options="tagOptions"
+          v-model="tags"
+          class="m-t-24"
+        />
+        <div class="flex justify-center m-t-24">
+          <BIMDataButton
+            color="primary"
+            ghost
+            radius
+            class="m-r-12"
+            @click="resetFilters"
+            >Réinitialiser</BIMDataButton
+          >
+          <BIMDataButton color="primary" fill radius @click="submitFilters"
+            ><BIMDataIcon
+              name="search"
+              fill
+              color="default"
+              margin="0 6px 0 0"
+              size="xxs"
+            /><span>Search</span></BIMDataButton
+          >
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from "vue";
+import { useToggle } from "@/composables/toggle";
+
+export default {
+  props: {
+    bcfTopics: {
+      type: Array,
+      required: true
+    }
+  },
+  emits: ["submit"],
+  setup(props, { emit }) {
+    const {
+      isOpen: showFilters,
+      close: closeFilters,
+      toggle: toggleFilters
+    } = useToggle();
+
+    // priority list
+    const priorities = ref([]);
+    const priorityOptions = computed(() => {
+      const uniquePriorityList = [
+        ...new Set(props.bcfTopics.map(bcfTopic => bcfTopic.priority))
+      ];
+      return uniquePriorityList.map(value =>
+        value === undefined ? "Non défini" : value
+      );
+    });
+
+    // status list
+    const status = ref([]);
+    const statusOptions = computed(() => {
+      const uniqueStatusList = [
+        ...new Set(props.bcfTopics.map(bcfTopic => bcfTopic.topicStatus))
+      ];
+      return uniqueStatusList.map(value =>
+        value === undefined ? "Non défini" : value
+      );
+    });
+
+    // tags list
+    const tags = ref([]);
+    const tagOptions = computed(() => {
+      return [...new Set(props.bcfTopics.flatMap(bcfTopic => bcfTopic.labels))];
+    });
+
+    const submitFilters = () => {
+      emit("submit", {
+        priorities: priorities.value,
+        status: status.value,
+        tags: tags.value
+      });
+      showFilters.value = false;
+    };
+
+    const resetFilters = () => {
+      priorities.value = [];
+      status.value = [];
+      tags.value = [];
+
+      emit("submit", {
+        priorities: priorities.value,
+        status: status.value,
+        tags: tags.value
+      });
+    };
+
+    return {
+      // References
+      priorities,
+      priorityOptions,
+      status,
+      statusOptions,
+      tags,
+      tagOptions,
+      showFilters,
+      // Methods
+      resetFilters,
+      submitFilters,
+      closeFilters,
+      toggleFilters
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss" src="./BcfFilters.scss"></style>
--- src/state/bcf.js
+++ src/state/bcf.js
@@ -8,14 +8,26 @@ const state = reactive({
 const loadBcfTopics = async project => {
   const topics = await BcfService.fetchProjectTopics(project);
 
-  const topicsWithSnapshots = await Promise.all(
-    topics.map(async topic => {
-      const viewpoints = await BcfService.fetchTopicViewpoints(project, topic);
-      return {
-        ...topic,
-        snapshots: viewpoints.map(viewpoint => viewpoint.snapshot)
-      };
-    })
+  let topicsWithSnapshots = [];
+
+  if (topics) {
+    topicsWithSnapshots = await Promise.all(
+      topics.map(async topic => {
+        const viewpoints = await BcfService.fetchTopicViewpoints(
+          project,
+          topic
+        );
+        return {
+          ...topic,
+          snapshots: viewpoints.map(viewpoint => viewpoint.snapshot),
+          components: viewpoints.map(viewpoint => viewpoint.components)
+        };
+      })
+    );
+  }
+
+  topicsWithSnapshots.sort((a, b) =>
+    a.creationDate < b.creationDate ? 1 : -1
   );
 
   state.bcfTopics = topicsWithSnapshots;
