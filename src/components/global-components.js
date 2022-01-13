2c2e368ce8238ec104f8f2a2852c64097fb66552
Gaelle <gaelle@bimdata.io>
2022-01-13 10:21:24 +0100

    add sort icons

--- src/components/global-components.js
+++ src/components/global-components.js
@@ -22,8 +22,14 @@ import AlphabeticalAscending from "@/components/images/AlphabeticalAscending.vue
 import AlphabeticalDescending from "@/components/images/AlphabeticalDescending.vue";
 import BillingsTableEmptyImage from "@/components/images/BillingsTableEmptyImage.vue";
 import BulletMarkImage from "@/components/images/BulletMarkImage.vue";
+import DateAscending from "@/components/images/DateAscending.vue";
+import DateDescending from "@/components/images/DateDescending.vue";
+import Grid from "@/components/images/Grid.vue";
+import IndexAscending from "@/components/images/IndexAscending.vue";
+import IndexDescending from "@/components/images/IndexDescending.vue";
 import InvitationAvatarImage from "@/components/images/InvitationAvatarImage.vue";
 import InvoicesTableEmptyImage from "@/components/images/InvoicesTableEmptyImage.vue";
+import List from "@/components/images/List.vue";
 import OrganizationFormSuccessImage from "@/components/images/OrganizationFormSuccessImage.vue";
 import ProPlanFormSuccessImage from "@/components/images/ProPlanFormSuccessImage.vue";
 import ProPlanInfoHeadingImage from "@/components/images/ProPlanInfoHeadingImage.vue";
@@ -55,8 +61,14 @@ export default {
   AlphabeticalDescending,
   BillingsTableEmptyImage,
   BulletMarkImage,
+  DateAscending,
+  DateDescending,
+  Grid,
+  IndexAscending,
+  IndexDescending,
   InvitationAvatarImage,
   InvoicesTableEmptyImage,
+  List,
   OrganizationFormSuccessImage,
   ProPlanFormSuccessImage,
   ProPlanInfoHeadingImage,
