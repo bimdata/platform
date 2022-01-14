0b41e04055944da6f7c9e30c873ff98a2ca5c3f1
Gaelle <gaelle@bimdata.io>
2022-01-14 18:38:54 +0100

    create CreateBcfTopic component

---
+++ src/components/specific/bcf/create-bcf-topic/CreateBcfTopic.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="create-bcf-topic flex items-center justify-between">
+    <div class="create-bcf-topic__header flex items-center">
+      <div>Index</div>
+      <div>Date</div>
+    </div>
+    <div class="create-bcf-topic__image"></div>
+    <div class="create-bcf-topic__content"></div>
+    <div class="create-bcf-topic__footer">
+      <BIMDataButton width="100%" color="primary" fill radius>
+        Valider
+      </BIMDataButton>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss" src="./CreateBcfTopic.scss"></style>
