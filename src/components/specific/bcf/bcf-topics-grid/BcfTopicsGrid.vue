91dcdd9ccd76de135607f4aa735b6a6c4d793882
LrxGaelle <gaelle@bimdata.io>
2022-01-14 18:43:13 +0100

    Fix/bcf bureautique view (#105)
    * fix OpenTopicIssue view :
    
    - fix disabled state on elements btn
    - fix display image block
    
    * add illustration for bcf without snapshot
    
    * Fix bcf topics grid view :
    
    - add illustration for bcf without snapshot
    - fix bcfTopic elements block
    - add openTopicIssue panel
    
    * fix BcfActionsCell view :
    
    - change transition name for OpenTopicIssue
    - add key for OpenTopicIssue panel
    - delete .guid inside openBcfTopic method
    
    * fix OpenTopicIssue view :
    
    - add illustration for topics without snapshot
    - add font-size for open-topic-issue panel
    
    * create BcfTopicsMetrics component
    
    * create AddBcf component
    
    * create EmptyBcfStats illustration
    
    * create CreateBcfTopic component
    
    * create CreateBcfTopicCard component
    
    * create EmptyBcfTopic illustration
    
    * fix ProjectBcf view :
    
    -  use AddBcf, BcfTopicsMetrics and CreateBcfTopicCard components
    - use transition grid and list for display topics

--- src/components/specific/bcf/bcf-topics-grid/BcfTopicsGrid.vue
+++ src/components/specific/bcf/bcf-topics-grid/BcfTopicsGrid.vue
@@ -9,7 +9,7 @@
           {{ bcfTopic.index }}
         </div>
         <div class="bcf-topic__header__infos__title flex items-center m-l-12">
-          <BIMDataTextBox maxWidth="100% - 48px)" :text="bcfTopic.title" />
+          <BIMDataTextBox maxWidth="100% - 48px" :text="bcfTopic.title" />
         </div>
       </div>
       <div class="bcf-topic__header__img flex items-center justify-center">
@@ -30,7 +30,7 @@
           :src="bcfTopic.snapshots[0].snapshotData"
           alt=""
         />
-        <BIMDataIcon v-else name="default" size="xxxl" fill color="default" />
+        <NoImgTopicBcf class="no-img-topic" v-else />
       </div>
     </div>
     <div class="bcf-topic__content p-12">
@@ -47,27 +47,74 @@
         <span v-else>Non renseigné</span>
       </div>
       <div class="flex justify-around m-t-12">
-        <BIMDataButton color="default" fill radius width="48%"
-          ><BIMDataIcon name="model3d" fill color="default" size="xs" /><span
-            class="m-6"
+        <BIMDataButton
+          color="default"
+          fill
+          radius
+          width="48%"
+          :disabled="
+            !bcfTopic.components.length ||
+            !bcfTopic.components[0].selection.length
+          "
+        >
+          <BIMDataIcon
+            name="model3d"
+            fill
+            color="default"
+            size="xs"
+            margin="0 6px 0 0"
+          />
+          <span
+            class="m-r-6"
+            v-if="
+              bcfTopic.components.length &&
+              bcfTopic.components[0].selection.length
+            "
             >{{ bcfTopic.components[0].selection.length }}</span
           >
-          {{
-            bcfTopic.components[0].selection.length ? "Elements" : "Element"
-          }}</BIMDataButton
-        >
-        <BIMDataButton color="primary" fill radius width="48%"
-          >Ouvrir</BIMDataButton
+          <span>
+            {{
+              bcfTopic.components.length &&
+              bcfTopic.components[0].selection.length
+                ? "Elements"
+                : "0 Element"
+            }}</span
+          >
+        </BIMDataButton>
+        <BIMDataButton
+          color="primary"
+          fill
+          radius
+          width="48%"
+          @click="openBcfTopic(bcfTopic)"
+          >Voir</BIMDataButton
         >
       </div>
     </div>
+    <transition name="slide-fade-left">
+      <div
+        v-show="showSidePanel"
+        key="bcf-topic-side-panel"
+        class="bcf-topic__side-panel"
+      >
+        <OpenTopicIssue :bcfTopic="bcfTopic" @close="showSidePanel = false" />
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
+
+import NoImgTopicBcf from "../../../images/NoImgTopicBcf.vue";
+
+import OpenTopicIssue from "../../../specific/bcf/bcf-topics-list/open-topic-issue/OpenTopicIssue.vue";
 
 export default {
+  components: {
+    NoImgTopicBcf,
+    OpenTopicIssue
+  },
   props: {
     project: {
       type: Object,
@@ -80,14 +127,29 @@ export default {
   },
   setup(props) {
     const getPriorityClasses = ref("");
-    const getStatusClasses = ref("");
     if (props.bcfTopic.priority) {
       getPriorityClasses.value = props.bcfTopic.priority.toLowerCase();
     }
+
+    const getStatusClasses = ref("");
     if (props.bcfTopic.topicStatus) {
       getStatusClasses.value = props.bcfTopic.topicStatus.toLowerCase();
     }
-    return { getPriorityClasses, getStatusClasses };
+
+    const showSidePanel = ref(false);
+    const bcfTopicToOpen = reactive({});
+    const openBcfTopic = topic => {
+      showSidePanel.value = true;
+      bcfTopicToOpen.value = topic;
+    };
+
+    return {
+      getPriorityClasses,
+      getStatusClasses,
+      bcfTopicToOpen,
+      showSidePanel,
+      openBcfTopic
+    };
   }
 };
 </script>
---
+++ src/components/specific/bcf/create-bcf-topic-card/CreateBcfTopicCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <BIMDataCard class="create-bcf-topic flex items-center justify-center">
+    <template #content>
+      <EmptyBcfTopic />
+      <p class="color-primary m-y-24">Créer un nouveau topic BCF</p>
+      <BIMDataButton
+        width="40px"
+        height="40px"
+        color="default"
+        fill
+        radius
+        icon
+      >
+        <BIMDataIcon name="plus" fill color="default" size="m" />
+      </BIMDataButton>
+    </template>
+  </BIMDataCard>
+</template>
+
+<script>
+import EmptyBcfTopic from "./EmptyBcfTopic.vue";
+export default {
+  components: {
+    EmptyBcfTopic
+  }
+};
+</script>
+
+<style scoped lang="scss" src="./CreateBcfTopicCard.scss"></style>
