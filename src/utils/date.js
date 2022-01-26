14951295d516a4a17efc831e5273d4ae4b829232
LrxGaelle <gaelle@bimdata.io>
2022-01-26 09:51:52 +0100

    Feature/date filters (#112)
    * add date.js file
    
    * add filter by start and end topic date

---
+++ src/utils/date.js
@@ -0,0 +1,30 @@
+// format input: dd/mm/yyy
+function formatToDateObject(date) {
+  const decomposedDate = date.split("/");
+
+  const output = new Date(
+    +decomposedDate[2],
+    decomposedDate[1] - 1,
+    +decomposedDate[0]
+  );
+
+  output.setHours(0, 0, 0, 0);
+
+  return output;
+}
+
+const regexDate = new RegExp(
+  /^([0]?[1-9]|[1|2][0-9]|[3][0|1])[./-]([0]?[1-9]|[1][0-2])[./-]([0-9]{4})$/
+);
+
+// from dd/mm/yyy (string) to yyyy-mm-dd (date Object)
+const formatDate = date => new Date(date.split("/").reverse().join("-"));
+const formatDateDDMMYYY = date => {
+  function pad(s) {
+    return s < 10 ? "0" + s : s;
+  }
+  const d = new Date(date);
+  return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join("/");
+};
+
+export { formatToDateObject, formatDate, formatDateDDMMYYY, regexDate };
--- src/views/project-board/project-bcf/composables/filter.js
+++ src/views/project-board/project-bcf/composables/filter.js
@@ -4,13 +4,17 @@ function useFilter(topics) {
   const filters = reactive({
     priorities: [],
     status: [],
-    tags: []
+    tags: [],
+    startDate: "",
+    endDate: ""
   });
 
   function onFiltersSubmit(submittedFilters) {
     filters.priorities = submittedFilters.priorities;
     filters.status = submittedFilters.status;
     filters.tags = submittedFilters.tags;
+    filters.startDate = submittedFilters.startDate;
+    filters.endDate = submittedFilters.endDate;
   }
 
   const filteredTopics = computed(() =>
@@ -29,6 +33,27 @@ function useFilter(topics) {
           filters.tags.length === 0 ||
           filters.tags.some(tag => topic.labels.includes(tag))
       )
+      .filter(topic => {
+        if (filters.startDate && filters.endDate) {
+          const formatStartTopicDate = filters.startDate
+            .split("/")
+            .reverse()
+            .join("-");
+          const formatEndTopicDate = filters.endDate
+            .split("/")
+            .reverse()
+            .join("-");
+          // add T for filter on one entire day
+          const startTopicDate = new Date(formatStartTopicDate + "T00:00");
+          const endTopicDate = new Date(formatEndTopicDate + "T23:59:59");
+          return (
+            startTopicDate <= topic.creationDate &&
+            topic.creationDate <= endTopicDate
+          );
+        }
+
+        return true;
+      })
   );
 
   return {
