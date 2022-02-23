6d96c9ecc2096f835ee832e0fb56a47c5ef15ef2
Paul Vilalta <vilaltapaul@gmail.com>
2022-02-23 15:08:02 +0100

    implementation of GuidedTour component

---
+++ src/config/guidedTour.js
@@ -0,0 +1,21 @@
+const steps = [
+  {
+    target: "[data-guide=space-button]",
+    title: "Step 1",
+    content:
+      "Great, you are now on the IFC list, click on the view button to display the IFC"
+  },
+  {
+    target: "[data-guide=project-button]",
+    clickable: true,
+    title: "Step 2",
+    content: "here is the IFC, click on next to continue"
+  },
+  {
+    target: "[data-guide=project-input-search]",
+    title: "Step 3",
+    content: "here is the search bar to find your project"
+  }
+];
+
+export default steps;
