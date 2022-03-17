e59f882c94eefe17e1234f79329a317a815d3dd6
amineau <antoine@bimdata.io>
2022-03-17 09:59:46 +0100

    deploy dev branches (#168)
    * create workflows to create and delete deployment, add an action to edit title pr
    
    * ci: fix key file
    
    * ci: debug
    
    * user current kubernetes image
    
    * fix: remove tabs from description

---
+++ .github/actions/pr-title-update/src/main.js
@@ -0,0 +1,48 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+function updatePR({token, body, pull_number, owner, repo}) {
+  const octokit = github.getOctokit(token);
+  return octokit.rest.pulls.update({
+    owner, repo, pull_number, body,
+  });
+}
+
+async function run() {
+  try {
+    const token = process.env["GITHUB_TOKEN"];
+    if (!token) {
+      core.setFailed("Requires: GITHUB_TOKEN");
+      return;
+    }
+
+    if (github.context.eventName !== "pull_request") {
+      core.info(
+        `Skipping PR: this isn't a pull_request event for us to handle`
+      );
+      return;
+    }
+
+    if (github.context.payload.action !== "opened") {
+      core.info(
+        `Skipping PR: this PR was already updated with a base branch label`
+      );
+      return;
+    }
+
+    const platform_url = core.getInput("platform_url", { required: true });
+    const body = `${platform_url}
+
+${github.context.payload.pull_request.body}`;
+    const pull_number = github.context.payload.pull_request.number;
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+
+    await updatePR({token, body, pull_number, owner, repo});
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
