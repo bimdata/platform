e07808fc81fb4f77be5ea2260b56c58e50529327
Paul Vilalta <vilaltapaul@gmail.com>
2022-04-05 15:28:56 +0200

    scenario fully implemented

--- src/config/guidedTour/guidedTour.js
+++ src/config/guidedTour/guidedTour.js
@@ -2,6 +2,7 @@ import i18n from "../../i18n/index.js";
 import IntroOutroPlatform from "./platform/IntroOutroPlatform.vue";
 
 const { t } = i18n.global;
+const imgPath = "/static/guidedTour/platform/";
 
 const tours = [
   {
@@ -11,7 +12,7 @@ const tours = [
         props: {
           title: t("GuidedTour.platform.intro.title"),
           content: t("GuidedTour.platform.intro.content"),
-          img: "/static/guidedTour/platform/draw_intro.png"
+          img: imgPath + "intro.png"
         },
         layout: IntroOutroPlatform
       },
@@ -20,7 +21,10 @@ const tours = [
         target: ["btn-spaces", "btn-projects", "btn-subscriptions"],
         props: {
           title: t("GuidedTour.platform.actionButtons.title"),
-          content: t("GuidedTour.platform.actionButtons.content")
+          content: t("GuidedTour.platform.actionButtons.content"),
+          img: imgPath + "actionButtons.gif",
+          imgPosition: "96% 50%", // horizontal vertical
+          imgSize: "200%"
         }
       },
       {
@@ -28,7 +32,10 @@ const tours = [
         target: "dashboard-space",
         props: {
           title: t("GuidedTour.platform.firstSpace.title"),
-          content: t("GuidedTour.platform.firstSpace.content")
+          content: t("GuidedTour.platform.firstSpace.content"),
+          img: imgPath + "firstSpace.gif",
+          imgPosition: "-13% 48%",
+          imgSize: "150%"
         }
       },
       {
@@ -37,7 +44,10 @@ const tours = [
         target: "dashboard-project",
         props: {
           title: t("GuidedTour.platform.firstProject.title"),
-          content: t("GuidedTour.platform.firstProject.content")
+          content: t("GuidedTour.platform.firstProject.content"),
+          img: imgPath + "firstProject.gif",
+          imgPosition: "49% 50%",
+          imgSize: "77%"
         }
       },
       {
@@ -45,7 +55,10 @@ const tours = [
         target: "preview-ifc",
         props: {
           title: t("GuidedTour.platform.previewIfc.title"),
-          content: t("GuidedTour.platform.previewIfc.content")
+          content: t("GuidedTour.platform.previewIfc.content"),
+          img: imgPath + "modelsOverview.gif",
+          imgPosition: "72% 50%",
+          imgSize: "233%"
         }
       },
       {
@@ -53,7 +66,10 @@ const tours = [
         target: "users-manager",
         props: {
           title: t("GuidedTour.platform.usersManager.title"),
-          content: t("GuidedTour.platform.usersManager.content")
+          content: t("GuidedTour.platform.usersManager.content"),
+          img: imgPath + "usersManager.gif",
+          imgPosition: "126% 108%",
+          imgSize: "98%"
         }
       },
       {
@@ -61,7 +77,10 @@ const tours = [
         target: "models-manager",
         props: {
           title: t("GuidedTour.platform.modelsManager.title"),
-          content: t("GuidedTour.platform.modelsManager.content")
+          content: t("GuidedTour.platform.modelsManager.content"),
+          img: imgPath + "modelsManager.gif",
+          imgPosition: "74% 38%",
+          imgSize: "105%"
         }
       },
       {
@@ -71,7 +90,10 @@ const tours = [
         targetDetail: "> ul > li:nth-child(2)",
         props: {
           title: t("GuidedTour.platform.gedButton.title"),
-          content: t("GuidedTour.platform.gedButton.content")
+          content: t("GuidedTour.platform.gedButton.content"),
+          img: imgPath + "gedButton.gif",
+          imgPosition: "-5% 54%",
+          imgSize: "300%"
         }
       },
       {
@@ -79,7 +101,10 @@ const tours = [
         target: ["btn-new-folder", "btn-upload-file", "file-tree"],
         props: {
           title: t("GuidedTour.platform.fileTree.title"),
-          content: t("GuidedTour.platform.fileTree.content")
+          content: t("GuidedTour.platform.fileTree.content"),
+          img: imgPath + "fileTree.gif",
+          imgPosition: "-16% 62%",
+          imgSize: "167%"
         }
       },
       {
@@ -87,7 +112,10 @@ const tours = [
         target: "btn-manage-groups",
         props: {
           title: t("GuidedTour.platform.groupManager.title"),
-          content: t("GuidedTour.platform.groupManager.content")
+          content: t("GuidedTour.platform.groupManager.content"),
+          img: imgPath + "groupManager.gif",
+          imgPosition: "45% -18%",
+          imgSize: "96%"
         }
       },
       {
@@ -98,7 +126,10 @@ const tours = [
         targetDetail: "> .breadcrumb-selector > div > div",
         props: {
           title: t("GuidedTour.platform.changeSpace.title"),
-          content: t("GuidedTour.platform.changeSpace.content")
+          content: t("GuidedTour.platform.changeSpace.content"),
+          img: imgPath + "changeSpace.gif",
+          imgPosition: "-10% 77%",
+          imgSize: "210%"
         }
       },
       {
@@ -107,7 +138,7 @@ const tours = [
         props: {
           title: t("GuidedTour.platform.outro.title"),
           content: t("GuidedTour.platform.outro.content"),
-          img: "/static/guidedTour/platform/draw_outro.png"
+          img: imgPath + "outro.png"
         },
         layout: IntroOutroPlatform
       }
