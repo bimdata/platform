3dc23fd73e9585f92fd776ffee1649aaeafc9add
Gaelle <gaelle@bimdata.io>
2022-04-08 10:53:17 +0200

    use css variables for graph icon

--- src/components/images/Graph.vue
+++ src/components/images/Graph.vue
@@ -15,13 +15,24 @@
     />
     <path
       d="M21.8824 12.2281C21.8824 6.89506 17.5592 2.57178 12.2261 2.57178C11.6725 2.57178 11.1297 2.61837 10.6016 2.70785"
-      stroke="#F8C62C"
+      :stroke="activeColor"
       stroke-width="5"
     />
     <path
       d="M12.2266 21.8843C6.89359 21.8843 2.57031 17.5611 2.57031 12.228C2.57031 7.44861 6.04256 3.4802 10.6021 2.70776"
-      stroke="#2F374A"
+      class="stroke-primary"
       stroke-width="5"
     />
   </svg>
 </template>
+
+<script>
+export default {
+  props: {
+    activeColor: {
+      type: String,
+      default: "var(--color-secondary)"
+    }
+  }
+};
+</script>
