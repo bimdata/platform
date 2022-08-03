61804c552f62a825da479ad0ffedd8b52d105874
NicolasRichel <nicolas@bimdata.io>
2022-08-03 15:47:47 +0200

    fix(test): disable grant assertion on signup

--- tests/e2e/specs/signup/signup.cy.js
+++ tests/e2e/specs/signup/signup.cy.js
@@ -11,9 +11,9 @@ describe("Signup Test", () => {
     cy.get("input[name=password2]").type(Cypress.env("USER_PASSWORD"));
     cy.get("input[type=submit]").click();
 
-    cy.url({timeout: 15000}).should("contain", Cypress.env("IAM_BASE_URL")); // wait for the creation of the demo
+    // cy.url().should("contain", Cypress.env("IAM_BASE_URL"));
 
-    cy.get("input[type=submit][value=Yes]").click();
+    // cy.get("input[type=submit][value=Yes]").click();
 
     cy.url().should("contain", Cypress.config("baseUrl"));
   });
