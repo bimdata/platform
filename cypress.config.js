b50ddc3ea775fc42f95a1ebae9496116b7c2b496
NicolasRichel <nicolas@bimdata.io>
2022-08-03 14:56:19 +0200

    fix(test): update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -6,10 +6,11 @@ module.exports = defineConfig({
   screenshotsFolder: "tests/e2e/screenshots",
   videosFolder: "tests/e2e/videos",
   e2e: {
-    baseUrl: "https://platform-dev-e2e-tests.bimdata.io",
+    baseUrl: "https://platform.bimdata.test",
     specPattern: [
+      // Execute "signup tests" first in order to create test users
       "tests/e2e/specs/signup/*.cy.{js,jsx,ts,tsx}",
-      "tests/e2e/specs/*.cy.{js,jsx,ts,tsx}",
+      "tests/e2e/specs/*.cy.{js,jsx,ts,tsx}"
     ],
     supportFile: "tests/e2e/support/index.js",
     experimentalSessionAndOrigin: true
