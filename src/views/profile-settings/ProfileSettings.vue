6f78f882784d1c1c7a02d725a6d559323ab5015c
Paul Vilalta <vilaltapaul@gmail.com>
2022-08-26 16:32:35 +0200

    add handle delete

--- src/views/profile-settings/ProfileSettings.vue
+++ src/views/profile-settings/ProfileSettings.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="profile-settings">
+  <template v-if="deleteLoader"
+    ><BIMDataLoading
+      :message="$t('ProfileSettings.loading.message')"
+      :subMessage="$t('ProfileSettings.loading.subMessage')"
+  /></template>
+  <div v-show="!deleteLoader" class="profile-settings">
     <div class="profile-settings__back-btn">
       <GoBackButton />
     </div>
     <div class="profile-settings__container">
       <span>{{ $t("ProfileSettings.title") }}</span>
       <div class="profile-settings__container__content">
-        <BIMDataSpinner></BIMDataSpinner>
-        <iframe src="http://localhost:8000/embed_profile/" />
+        <BIMDataSpinner />
+        <template v-if="displayIframe">
+          <iframe ref="iframe" src="http://localhost:8000/embed_profile/" />
+        </template>
       </div>
     </div>
     <div class="profile-settings__ghost-element" />
@@ -15,7 +22,10 @@
 </template>
 
 <script>
+import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
+import { useAuth } from "@/state/auth.js";
+
 import GoBackButton from "@/components/specific/app/go-back-button/GoBackButton.vue";
 export default {
   components: {
@@ -23,8 +33,32 @@ export default {
   },
   setup() {
     const router = useRouter();
+    const { signOut } = useAuth();
+
+    const iframe = ref(null);
+    const deleteLoader = ref(false);
+    const displayIframe = ref(true);
+
+    onMounted(() => {
+      window.addEventListener("message", async message => {
+        switch (message.data) {
+          case "profile update successful":
+            displayIframe.value = false;
+            await signOut();
+            break;
+          case "account delete start":
+            setTimeout(() => {
+              deleteLoader.value = true;
+              iframe.value.onload = async () => await signOut();
+            }, 500);
+        }
+      });
+    });
+
     return {
-      // methods
+      iframe,
+      deleteLoader,
+      displayIframe,
       getBack: () => router.back()
     };
   }
