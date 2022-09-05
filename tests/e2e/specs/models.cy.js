665e1fcc564eca81710f8ce90a1f4ece87f8510a
NicolasRichel <nicolas@bimdata.io>
2022-09-05 10:57:16 +0200

    feat(test): add test hooks to model actions menu and files table

--- tests/e2e/specs/models.cy.js
+++ tests/e2e/specs/models.cy.js
@@ -12,9 +12,32 @@ describe("Models features", () => {
     cy.hook("btn-upload-model").click();
     cy.get("body > input[type=file]").selectFile("@ifcModel", { force: true });
 
-    cy.hook("models-table").within(() => {
-      cy.contains("tr", "ifc-model.ifc").should("have.length", 1);
-    });
+    // Assert that the new model appears in models table
+    cy.hook("models-table").contains("tr", "ifc-model.ifc").should("have.length", 1);
+
+    // Assert that the new model appears in files table
+    cy.hook("project-tab-files").click();
+    cy.hook("files-table").contains("tr", "ifc-model.ifc").should("have.length", 1);
+  });
+
+  it("Should rename a model", () => {
+    // TODO
+  });
+
+  it("Should download a model", () => {
+    // TODO
+  });
+
+  it("Should archive a model", () => {
+    // TODO
+  });
+
+  it("Should unarchive a model", () => {
+    // TODO
+  });
+
+  it("Should delete a model", () => {
+    // TODO
   });
 
 });
