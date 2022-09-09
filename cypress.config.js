13ef07b199a5e46793de9f9af440876bec1f9cc9
NicolasRichel <nicolas@bimdata.io>
2022-09-09 10:21:10 +0200

    fix(test): update config to work both locally and in CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,18 @@
 const { defineConfig } = require("cypress");
-const { APP_BASE_URL } = require("./cypress.env.json");
+const { existsSync } = require("fs");
 const { generateLogins } = require("./tests/e2e/test-utils.js");
 
 const users = new Map();
 
+let baseUrl = process.env.CYPRESS_APP_BASE_URL;
+if (existsSync("./cypress.env.json")) {
+  baseUrl = require("./cypress.env.json").APP_BASE_URL;
+}
+
 module.exports = defineConfig({
+  // This is necessary to handle signup/login scenarios with redirection
   chromeWebSecurity: false,
+
   e2e: {
     specPattern: [
       // Execute "setup tests" first in order to create test users
@@ -22,7 +29,7 @@ module.exports = defineConfig({
     videosFolder: "tests/e2e/videos",
     experimentalSessionAndOrigin: true,
 
-    baseUrl: APP_BASE_URL,
+    baseUrl,
 
     setupNodeEvents(on) {
       on("task", {
