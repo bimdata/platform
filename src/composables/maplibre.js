8ff8c39c6896340f5c9d35ab27b098213d510473
Hugo Duroux <hugo@bimdata.io>
2022-09-20 09:25:20 +0200

    Feature/maplibre (#285)
    * replace mapbox by maplibre
    
    * replace mapbox token by maptiler token

--- src/composables/maplibre.js
+++ src/composables/maplibre.js
@@ -1,8 +1,8 @@
-import mapboxgl from "mapbox-gl";
+import maplibregl from "maplibre-gl";
 
-mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN;
+const MAP_TILER_TOKEN = process.env.VUE_APP_MAPTILER_TOKEN;
 
-export function useMapbox(containerID) {
+export function useMaplibre(containerID) {
   const loadMap = (longitude, latitude) => {
     if (!document.getElementById(containerID)) {
       // Do not try to load map if container element does not exist
@@ -11,14 +11,15 @@ export function useMapbox(containerID) {
 
     if (longitude && latitude) {
       try {
-        const map = new mapboxgl.Map({
+        const map = new maplibregl.Map({
           container: containerID,
-          style: "mapbox://styles/mapbox/light-v10",
+          style: "https://api.maptiler.com/maps/streets/style.json?key=" + MAP_TILER_TOKEN,
           center: [longitude, latitude],
           zoom: 15.5,
           pitch: 45,
           bearing: -17.6,
-          attributionControl: false
+          attributionControl: false,
+          antialias: true,
         });
 
         map.on("load", () => {
@@ -32,7 +33,7 @@ export function useMapbox(containerID) {
             {
               type: "fill-extrusion",
               id: "3d-buildings",
-              source: "composite",
+              source: "openmaptiles",
               "source-layer": "building",
               filter: ["==", "extrude", "true"],
               minzoom: 15,
@@ -62,7 +63,7 @@ export function useMapbox(containerID) {
             labelsLayer ? labelsLayer.id : undefined
           );
 
-          new mapboxgl.Marker({ color: "#2F374A" /* color primary */ })
+          new maplibregl.Marker({ color: "#2F374A" /* color primary */ })
             .setLngLat([longitude, latitude])
             .addTo(map);
         });
