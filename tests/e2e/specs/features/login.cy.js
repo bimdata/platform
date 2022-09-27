5bedb2b090837175ea1cc29408a55c80f85fe3cd
NicolasRichel <nicolas@bimdata.io>
2022-09-27 15:36:19 +0200

    fix(test): add login test with 60s timeout top setup

--- tests/e2e/specs/features/login.cy.js
+++ tests/e2e/specs/features/login.cy.js
@@ -1,22 +1,22 @@
 describe("Login / Logout features", () => {
 
-  it("Should login & logout successfully", () => {
-    cy.task("get-user", "user0").then(({ email, password }) => {
-      cy.visit("/");
+  // it("Should login & logout successfully", () => {
+  //   cy.task("get-user", "user0").then(({ email, password }) => {
+  //     cy.visit("/");
   
-      cy.get("input[name=email]").type(email);
-      cy.get("input[name=password]").type(password);
-      cy.get("input[type=submit]").click();
+  //     cy.get("input[name=email]").type(email);
+  //     cy.get("input[name=password]").type(password);
+  //     cy.get("input[type=submit]").click();
   
-      // Assert login success
-      cy.url().should("contain", Cypress.env("APP_BASE_URL"));
+  //     // Assert login success
+  //     cy.url().should("contain", Cypress.env("APP_BASE_URL"));
   
-      cy.hook("btn-toggle-app-menu").click();
-      cy.hook("btn-logout").click();
+  //     cy.hook("btn-toggle-app-menu").click({ timeout: 60000 });
+  //     cy.hook("btn-logout").click();
   
-      // Assert logout success
-      cy.url().should("contain", `${Cypress.env("AUTH_BASE_URL")}/accounts/login`);
-    });
-  });
+  //     // Assert logout success
+  //     cy.url().should("contain", `${Cypress.env("AUTH_BASE_URL")}/accounts/login`);
+  //   });
+  // });
 
 });
--- tests/e2e/specs/setup/setup.cy.js
+++ tests/e2e/specs/setup/setup.cy.js
@@ -11,8 +11,27 @@ describe("E2E Tests Setup", () => {
     cy.createUser({ key: "user2", firstname: "David", lastname: "Hume" });
   });
 
-  it("Wait a moment for accounts creation", () => {
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(20000);
+  // it("Wait a moment for accounts creation", () => {
+  //   // eslint-disable-next-line cypress/no-unnecessary-waiting
+  //   cy.wait(20000);
+  // });
+
+  it("Should login & logout successfully", () => {
+    cy.task("get-user", "user0").then(({ email, password }) => {
+      cy.visit("/");
+  
+      cy.get("input[name=email]").type(email);
+      cy.get("input[name=password]").type(password);
+      cy.get("input[type=submit]").click();
+  
+      // Assert login success
+      cy.url().should("contain", Cypress.env("APP_BASE_URL"));
+  
+      cy.hook("btn-toggle-app-menu").click({ timeout: 60000 });
+      cy.hook("btn-logout").click();
+  
+      // Assert logout success
+      cy.url().should("contain", `${Cypress.env("AUTH_BASE_URL")}/accounts/login`);
+    });
   });
 });
