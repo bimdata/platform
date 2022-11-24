fe57cf0dfac63b2b65685ed9a1c5f8e1bd381675
NicolasRichel <nicolas@bimdata.io>
2022-11-24 09:36:09 +0100

    wip: create document-viewer component

---
+++ src/components/specific/files/document-viewer/DocumentViewer.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  folder: {
+    type: Object,
+    required: true
+  },
+  document: {
+    type: Object
+  }
+});
+</script>
+
+<template>
+  <div class="document-viewer">
+    <!-- TODO -->
+  </div>
+</template>
+
+<style scoped lang="scss" src="./DocumentViewer.scss"></style>
