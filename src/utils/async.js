246781496ebd4c227c09aa703b9ac861b7d048de
NicolasRichel <nicolas@bimdata.io>
2023-03-13 14:35:01 +0100

    fix(utils): use explicit args forwarding in debounce utils

--- src/utils/async.js
+++ src/utils/async.js
@@ -4,9 +4,9 @@ function delay(ms) {
 
 function debounce(callback, ms) {
   let timeoutID = null;
-  return function () {
+  return (...args) => {
     clearTimeout(timeoutID);
-    timeoutID = setTimeout(() => callback(...arguments), ms);
+    timeoutID = setTimeout(() => callback(...args), ms);
   };
 }
 
