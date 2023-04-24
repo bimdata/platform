8a892e6f34cd66cc1acf62347f0f6d56fb35ca90
NicolasRichel <nicolas@bimdata.io>
2023-04-24 14:23:11 +0200

    feat(upload): add e2e tests for upload retry

--- tests/e2e/specs/features/06_docimport.cy.js
+++ tests/e2e/specs/features/06_docimport.cy.js
@@ -63,4 +63,49 @@ describe("Documents import feature", () => {
     cy.deleteGedDoc("house-plan.pdf");
     cy.deleteGedDoc("terminaux-plafond.dwg");
   });
+
+  it("Should retry upload if it fails", () => {
+    // Arrange
+    // - Simulate an error from the API
+    cy.intercept(
+      { method: "POST", url: "**/document", times: 1 },
+      { statusCode: 500 }
+    );
+    // - Simulate an error from the network connection
+    cy.intercept(
+      { method: "POST", url: "**/document", times: 1 },
+      { forceNetworkError: true }
+    );
+
+    // Act
+    cy.hook("btn-upload-file").click();
+    cy.get("body > input[type=file]").selectFile("@housePlan", { force: true });
+
+    // Assert
+    cy.hook("files-table")
+      .contains("tbody tr", "house-plan.pdf")
+      .should("have.length", 1);
+
+    // Clean
+    cy.deleteGedDoc("house-plan.pdf");
+  });
+
+  it("Should retry upload at most 3 times", () => {
+    // Arrange
+    cy.intercept(
+      { method: "POST", url: "**/document" },
+      { statusCode: 500 }
+    ).as("uploadRequest");
+
+    // Act
+    cy.hook("btn-upload-file").click();
+    cy.get("body > input[type=file]").selectFile("@housePlan", { force: true });
+
+    // Assert
+    for (let i = 0; i < 4; i++) cy.wait("@uploadRequest");
+    cy.hook("files-table").find("thead tr .upload-card.failed");
+    cy.hook("files-table")
+      .contains("tbody tr", "house-plan.pdf")
+      .should("have.length", 0);
+  });
 });
