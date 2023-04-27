1a78813955609ff355ea3626a02ed33a9b4cbe01
Hugo Duroux <hugo@bimdata.io>
2023-04-27 15:09:02 +0200

    add preview for office files (#375)
    * add preview for office files
    
    * improve code derundancy
    
    * force fetch preview when we open a non defined preview

--- src/config/files.js
+++ src/config/files.js
@@ -12,4 +12,8 @@ const FILE_TYPE = Object.freeze({
 
 const STANDARD_IGNORED_FILES = Object.freeze([".DS_Store"]);
 
-export { FILE_PERMISSION, FILE_TYPE, STANDARD_IGNORED_FILES };
+const OFFICE_FILES = [".ppt", ".pptx", ".odp", ".xls", ".xlsx", ".ods", ".doc", ".docx", ".odt"];
+
+const IMAGE_PREVIEW_FILES = [".apng", ".avif", ".gif", ".jpeg", ".jpg", ".png", ".svg", ".webp"];
+
+export { FILE_PERMISSION, FILE_TYPE, STANDARD_IGNORED_FILES, OFFICE_FILES, IMAGE_PREVIEW_FILES };
