d58723cca07f24a3b11f7974abf72484efc40d9d
NicolasRichel <nicolas@bimdata.io>
2023-09-05 17:45:05 +0200

    fix: rewrite MainModelSelector as a setup function component (instead of script setup)

--- src/components/specific/models/models-card/main-model-selector/MainModelSelector.vue
+++ src/components/specific/models/models-card/main-model-selector/MainModelSelector.vue
@@ -1,70 +1,3 @@
-<script setup>
-import { ref, watch } from "vue";
-import { MODEL_TYPE } from "../../../../../config/models.js";
-import { useModels } from "../../../../../state/models.js";
-import { useProjects } from "../../../../../state/projects.js";
-
-const { IFC, POINT_CLOUD } = MODEL_TYPE;
-const { loadProjectModels } = useModels();
-const { updateProject } = useProjects();
-
-const props = defineProps({
-  project: {
-    type: Object,
-    required: true
-  },
-  models: {
-    type: Array,
-    required: true
-  },
-  onCancel: {
-    type: Function,
-    required: true
-  },
-  onSuccess: {
-    type: Function,
-    required: true
-  },
-  onError: {
-    type: Function,
-    default: () => console.error
-  }
-});
-
-const loading = ref(false);
-const selectedModel = ref(null);
-
-watch(
-  () => props.models,
-  () => {
-    selectedModel.value =
-      props.models.find(m => m.id === props.project.main_model_id) ??
-      props.models[0];
-  },
-  { immediate: true }
-);
-
-const cancel = () => {
-  props.onCancel();
-};
-
-const submit = async () => {
-  try {
-    loading.value = true;
-    await updateProject({
-      ...props.project,
-      main_model_id: selectedModel.value.id
-    });
-    await loadProjectModels(props.project);
-    props.onSuccess();
-  } catch (error) {
-    props.onError(error);
-  } finally {
-    loading.value = false;
-  }
-};
-</script>
-
 <template>
   <div class="main-model-selector">
     <div v-if="loading" class="main-model-selector__loading">
@@ -110,5 +43,82 @@ const submit = async () => {
     </div>
   </div>
 </template>
+
+<script>
+import { ref, watch } from "vue";
+import { MODEL_TYPE } from "../../../../../config/models.js";
+import { useModels } from "../../../../../state/models.js";
+import { useProjects } from "../../../../../state/projects.js";
+
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true
+    },
+    models: {
+      type: Array,
+      required: true
+    },
+    onCancel: {
+      type: Function,
+      required: true
+    },
+    onSuccess: {
+      type: Function,
+      required: true
+    }
+  },
+  setup(props) {
+    const { IFC, POINT_CLOUD } = MODEL_TYPE;
+    const { loadProjectModels } = useModels();
+    const { updateProject } = useProjects();
+
+    const loading = ref(false);
+    const selectedModel = ref(null);
+
+    watch(
+      () => props.models,
+      () => {
+        selectedModel.value =
+          props.models.find(m => m.id === props.project.main_model_id) ??
+          props.models[0];
+      },
+      { immediate: true }
+    );
+
+    const cancel = () => {
+      props.onCancel();
+    };
+
+    const submit = async () => {
+      try {
+        loading.value = true;
+        await updateProject({
+          ...props.project,
+          main_model_id: selectedModel.value.id
+        });
+        await loadProjectModels(props.project);
+        props.onSuccess();
+      } catch (error) {
+        console.error(error);
+      } finally {
+        loading.value = false;
+      }
+    };
+
+    return {
+      // References
+      loading,
+      selectedModel,
+      IFC,
+      POINT_CLOUD,
+      // Methods
+      cancel,
+      submit
+    };
+  }
+};
+</script>
 
 <style scoped lang="scss" src="./MainModelSelector.scss"></style>
