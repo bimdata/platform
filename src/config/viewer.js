d872bd9cdb3c895d6792c675685b5e2073c5d8ef
Amoki <hugo@bimdata.io>
2023-09-29 11:20:59 +0200

    remvove deprecated plugins and update idex

--- src/config/viewer.js
+++ src/config/viewer.js
@@ -1,13 +1,6 @@
 const AVAILABLE_PLUGINS = Object.freeze({
-  backgroundColor:
-    "https://unpkg.com/@bimdata/background-color-viewer-plugin@1.0.1",
-  bimobject: "https://unpkg.com/@bimdata/bimobject-viewer-plugin@1.0.2",
-  gltfExtractor:
-    "https://unpkg.com/@bimdata/gltf-extractor-viewer-plugin@1.0.2",
-  idex: "https://unpkg.com/@bimdata/idex-viewer-plugin@1.0.8",
+  idex: "https://unpkg.com/@bimdata/idex-viewer-plugin@2.0.0",
   iot: "https://unpkg.com/@bimdata/iot-viewer-plugin@1.0.13",
-  realiz3D: "https://unpkg.com/@bimdata/realiz3d-viewer-plugin@0.0.2",
-  svgExtractor: "https://unpkg.com/@bimdata/svg-extractor-viewer-plugin@1.0.2"
 });
 
 /**
