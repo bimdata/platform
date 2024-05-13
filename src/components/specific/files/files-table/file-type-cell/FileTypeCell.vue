151d8cf8e70acb2b288b7a1001b9f2840365187e
Gaelle <gaelle@bimdata.io>
2024-05-13 10:52:01 +0200

    refactoring code in FileTypeCell for merge conditionnal v-if isFolder

--- src/components/specific/files/files-table/file-type-cell/FileTypeCell.vue
+++ src/components/specific/files/files-table/file-type-cell/FileTypeCell.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="file-type-cell">
-    <BIMDataIcon
-      v-if="isFolder(file)"
-      :name="!project.isAdmin && file.user_permission < 100 ? 'readonlyFolder' : 'folder'"
-      size="m"
-      fill
-      color="primary"
-    />
-    <BIMDataFileIcon v-else :fileName="file.file_name" :size="20" />
-    <div v-if="isFolder(file)">
+    <template v-if="isFolder(file)">
+      <BIMDataIcon
+        v-if="isFolder(file)"
+        :name="!project.isAdmin && file.user_permission < 100 ? 'readonlyFolder' : 'folder'"
+        size="m"
+        fill
+        color="primary"
+      />
       <span v-if="file.children.length === 0" class="file-type-cell__folder-icon empty">0</span>
-      <span v-else class="file-type-cell__folder-icon not-empty">{{file.children.length}}</span>
-    </div>
+      <span v-else class="file-type-cell__folder-icon not-empty">{{ file.children.length }}</span>
+    </template>
+    <BIMDataFileIcon v-else :fileName="file.file_name" :size="20" />
   </div>
 </template>
 
@@ -26,7 +26,7 @@ export default {
     },
     project: {
       type: Object,
-      required: true
+      required: true,
     },
   },
   setup() {
