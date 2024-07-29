27a687a5645521e086cfa47cfdff087bce7bcdb5
Gaelle <gaelle@bimdata.io>
2024-07-29 09:16:31 +0200

    delete requestTimeout cypress config

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -22,7 +22,6 @@ module.exports = defineConfig({
   viewportHeight: 728,
 
   e2e: {
-    requestTimeout: 30000,
     supportFile: "tests/e2e/support/index.js",
     specPattern: [
       // Execute "setup tests" first in order to create test users
