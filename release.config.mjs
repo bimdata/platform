e314e723cea707d87257b4105b68e94aaee75247
Nicolas Richel <nicolas@bimdata.io>
2024-09-30 15:57:57 +0200

    chore(ci): setup semantic-release configuration (#475)

---
+++ release.config.mjs
@@ -0,0 +1,30 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { tag: "PATCH", release: "patch" },
+          { tag: "MINOR", release: "minor" },
+          { tag: "MAJOR", release: "major" },
+        ],
+        parserOpts: {
+          headerPattern: /^(PATCH|MINOR|MAJOR): (.*)$/,
+          headerCorrespondence: ["tag", "subject"],
+        },
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    "@semantic-release/git",
+  ],
+  branches: [
+    "main",
+    "master",
+  ],
+};
