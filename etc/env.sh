49be18820e2b0d05c8994770f31f753b15d38714
Romain <58464216+RomainMou@users.noreply.github.com>
2024-11-15 14:19:57 +0100

    chore: Add the resource integrity at runtime in the index html file. (#484)
    * Add the resource integrity at runtime in the index html file.
    
    * Modify withprevious Dockerfile too.

--- etc/env.sh
+++ etc/env.sh
@@ -2,8 +2,17 @@
 
 set -euo pipefail
 
-js_original_path="/usr/share/nginx/html/assets-original"
-js_path="/usr/share/nginx/html/assets"
+nginx_root="/usr/share/nginx/html"
+assets_original_folder="assets-original"
+assets_folder="assets"
+
+js_original_path="${nginx_root}/${assets_original_folder}"
+js_path="${nginx_root}/${assets_folder}"
+
+index_original_path="${nginx_root}/index.html-original"
+index_path="${nginx_root}/index.html"
+
+hash_algo="sha384"
 
 # Associative var_name => placeholder_value
 declare -A vars_to_placeholders=(
@@ -35,17 +44,54 @@ declare -A vars_to_placeholders=(
     [VUE_APP_NOTIFICATION_ENABLED]="ENV.VUE_APP_NOTIFICATION_ENABLED"
 )
 
-# Copy the original JS
-if [[ -d "${js_path}" ]] ; then
-    rm -r "${js_path}"
-fi
-cp -r "${js_original_path}" "${js_path}"
+# Function to compute the hash of a resource
+function compute_hash() {
+    local resource_path=$1
+    local hash=$(cat "${resource_path}" | openssl dgst -${hash_algo} -binary | openssl base64 -A)
+    echo "${hash}"
+}
 
-# For each env variable, if unset, set it to empty string and replace
-# the placeholder by the wanted value in the JS files
-for var_name in "${!vars_to_placeholders[@]}" ; do
-    if [[ -z ${!var_name+x} ]] ; then
-        declare ${var_name}=""
+function initialize() {
+    # Cleanup old files for example after a restart & copy fresh one in case variables changed
+    if [[ -d "${js_path}" ]] ; then
+        rm -r "${js_path}"
     fi
-    sed -i "s|${vars_to_placeholders[$var_name]}|\"${!var_name}\"|g" "${js_path}"/*
-done
+    cp -r "${js_original_path}" "${js_path}"
+
+    if [[ -f "${index_path}" ]] ; then
+        rm "${index_path}"
+    fi
+    cp "${index_original_path}" "${index_path}"
+}
+
+function customize_assets_for_env() {
+    for var_name in "${!vars_to_placeholders[@]}" ; do
+        if [[ -z ${!var_name+x} ]] ; then
+            declare ${var_name}=""
+        fi
+        sed -i "s|${vars_to_placeholders[$var_name]}|\"${!var_name}\"|g" "${js_path}"/*
+    done
+}
+
+function customize_index_resources_integrity() {
+    # Read the index file and find all the local css and js files
+    js_files=($(grep -oP "src=\"/${assets_folder}\K/[^\"]+\.js" "${index_path}"))
+    css_files=($(grep -oP "href=\"/${assets_folder}\K/[^\"]+\.css" "${index_path}"))
+
+    # Compute the hash of each file and update the index file with the integrity attribute
+    for js_file in "${js_files[@]}" ; do
+        js_file_path="${js_path}/${js_file}"
+        js_hash=$(compute_hash "${js_file_path}")
+        sed -i "s|src=\"/${assets_folder}${js_file}\"|src=\"/${assets_folder}${js_file}\" integrity=\"${hash_algo}-${js_hash}\"|g" ${index_path}
+    done
+    for css_file in "${css_files[@]}" ; do
+        css_file_path="${js_path}/${css_file}"
+        css_hash=$(compute_hash "${css_file_path}")
+        sed -i "s|href=\"/${assets_folder}${css_file}\"|href=\"/${assets_folder}${css_file}\" integrity=\"${hash_algo}-${css_hash}\"|g" ${index_path}
+    done
+}
+
+# Copy the original files
+initialize
+customize_assets_for_env
+customize_index_resources_integrity
