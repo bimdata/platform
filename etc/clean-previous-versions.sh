92eb3afcc106f38f5dd1336a38a51582f99c302a
RomainMou <58464216+RomainMou@users.noreply.github.com>
2025-07-04 09:33:35 +0200

    Fix comment to describe the last comportement of the script

--- etc/clean-previous-versions.sh
+++ etc/clean-previous-versions.sh
@@ -8,9 +8,9 @@ TIMESTAMP_TOLERANCE=120
 
 # -d: get directory information, not on its content
 # -l: long output,
-# --time-style=+@%s => time format is a timestamp
+# --time-style=+@%s@ => time format is a timestamp
 # the '@' are added arround the timestamp to be able to easly parse the output, that not trivial without it
-# | cut -d '@' -f 2- : keep what's after the @ we insert before the timestamp (timestamp & filename)
+# | cut -d '@' -f 2 : extract the timestamp with the help of our @
 last_build_timestamp=$(ls -dl --time-style=+@%s@ $ASSETS_PATH | cut -d '@' -f 2)
 
 # Just in case, we adjust the timestamps with an error margin, not sure if necessary
