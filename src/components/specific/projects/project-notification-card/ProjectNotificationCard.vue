38846b9e349adf238ffe82f9821bb72590e274b6
NicolasRichel <nicolas@bimdata.io>
2025-10-09 10:09:31 +0200

    fix: remove defineModel useless import

--- src/components/specific/projects/project-notification-card/ProjectNotificationCard.vue
+++ src/components/specific/projects/project-notification-card/ProjectNotificationCard.vue
@@ -97,7 +97,7 @@
 </template>
 
 <script setup>
-import { ref, defineModel, computed, watch } from "vue";
+import { ref, computed, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { useNotificationSchedule } from "../../../../state/notifications.js";
 import {
