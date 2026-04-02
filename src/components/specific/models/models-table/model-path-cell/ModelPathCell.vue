7b165fd4e30a09198165ea29d7f54094fbea1809
NicolasRichel <nicolas@bimdata.io>
2026-04-02 10:52:47 +0200

    PATCH: fix(models-manager): avoid error on Meta-Buildings tab

--- src/components/specific/models/models-table/model-path-cell/ModelPathCell.vue
+++ src/components/specific/models/models-table/model-path-cell/ModelPathCell.vue
@@ -1,78 +1,85 @@
 <template>
   <div class="model-path-cell" @mouseenter="hovering = true" @mouseleave="hovering = false">
-    <div class="model-path-cell__last-folder" v-click-away="() => (showFullPath = false)">
-      <BIMDataIconFolderLocation fill color="primary" margin="0 12px 0 0" />
-      <BIMDataTextbox
-        :text="lastFolderLocation(model.document_id)"
-        cutPosition="middle"
-        :tooltip="false"
-        width="auto"
-        maxWidth="200px"
-      />
-    </div>
-    <transition name="fade">
-      <div v-if="hovering || showFullPath" class="model-path-cell__location">
-        <div
-          v-if="truncatedFolderLocation(model.document_id).length > 0"
-          class="flex items-center"
-          v-for="(folder, index) in truncatedFolderLocation(model.document_id)"
-          :key="folder.id"
-        >
-          <div
-            class="folder-name flex items-center"
-            @click.stop="selectFile(folder)"
-            v-if="folder.id !== 'ellipsis'"
-          >
-            <BIMDataIconFolderLocation
-              v-if="index === truncatedFolderLocation(model.document_id).length - 1"
-              fill
-              color="primary"
-              margin="0 6px 0 0"
-            />
-            <BIMDataIconFolder v-else fill color="primary" margin="0 6px 0 0" />
-            <BIMDataTextbox
-              :text="folder.name"
-              cutPosition="middle"
-              tooltipPosition="bottom"
-              tooltipColor="primary"
-              width="auto"
-              maxWidth="200px"
-            >
-            </BIMDataTextbox>
-          </div>
-          <div v-if="folder.id === 'ellipsis'" class="folder-ellipsis flex items-center m-x-12">
-            <BIMDataTextbox
-              :text="folder.name"
-              cutPosition="middle"
-              tooltipPosition="bottom"
-              tooltipColor="primary"
-              width="auto"
-              maxWidth="200px"
-            >
-            </BIMDataTextbox>
-          </div>
-          <div v-if="showChevron(folder, index, model.document_id)" class="m-x-6">
-            <BIMDataIconChevron size="xxxs" fill color="default" />
-          </div>
-        </div>
-
-        <span
-          v-else
-          class="folder-name flex items-center"
-          @click.stop="goToGedRoot"
-        >
-          <BIMDataIconFolderLocation fill color="primary" margin="0 6px 0 0" />
-          {{ $t("t.rootFolder") }}
-        </span>
+    <template v-if="hasLocation">
+      <div class="model-path-cell__last-folder" v-click-away="() => (showFullPath = false)">
+        <BIMDataIconFolderLocation fill color="primary" margin="0 12px 0 0" />
+        <BIMDataTextbox
+          :text="lastFolderLocation(model.document_id)"
+          cutPosition="middle"
+          :tooltip="false"
+          width="auto"
+          maxWidth="200px"
+        />
       </div>
-    </transition>
+  
+      <transition name="fade">
+        <div v-if="hovering || showFullPath" class="model-path-cell__location">
+          <div
+            v-if="truncatedFolderLocation(model.document_id).length > 0"
+            class="flex items-center"
+            v-for="(folder, index) in truncatedFolderLocation(model.document_id)"
+            :key="folder.id"
+          >
+            <div
+              class="folder-name flex items-center"
+              @click.stop="selectFile(folder)"
+              v-if="folder.id !== 'ellipsis'"
+            >
+              <BIMDataIconFolderLocation
+                v-if="index === truncatedFolderLocation(model.document_id).length - 1"
+                fill
+                color="primary"
+                margin="0 6px 0 0"
+              />
+              <BIMDataIconFolder v-else fill color="primary" margin="0 6px 0 0" />
+              <BIMDataTextbox
+                :text="folder.name"
+                cutPosition="middle"
+                tooltipPosition="bottom"
+                tooltipColor="primary"
+                width="auto"
+                maxWidth="200px"
+              >
+              </BIMDataTextbox>
+            </div>
+            <div v-if="folder.id === 'ellipsis'" class="folder-ellipsis flex items-center m-x-12">
+              <BIMDataTextbox
+                :text="folder.name"
+                cutPosition="middle"
+                tooltipPosition="bottom"
+                tooltipColor="primary"
+                width="auto"
+                maxWidth="200px"
+              >
+              </BIMDataTextbox>
+            </div>
+            <div v-if="showChevron(folder, index, model.document_id)" class="m-x-6">
+              <BIMDataIconChevron size="xxxs" fill color="default" />
+            </div>
+          </div>
+  
+          <span
+            v-else
+            class="folder-name flex items-center"
+            @click.stop="goToGedRoot"
+          >
+            <BIMDataIconFolderLocation fill color="primary" margin="0 6px 0 0" />
+            {{ $t("t.rootFolder") }}
+          </span>
+        </div>
+      </transition>
+    </template>
+    <template v-else>
+      -
+    </template>
   </div>
 </template>
 
 <script>
-import { ref, onDeactivated, onActivated } from "vue";
+import { computed, onActivated, onDeactivated, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { FILE_TYPE } from "../../../../../config/files.js";
+import { MODEL_TYPE } from "../../../../../config/models.js";
 import { useFiles } from "../../../../../state/files.js";
 import { getAscendants } from "../../../../../utils/file-structure.js";
 
@@ -92,12 +99,15 @@ export default {
     const { t } = useI18n();
     const { fileStructureHandler: handler } = useFiles();
 
+    const hovering = ref(false);
+    const showFullPath = ref(false);
+    const hasLocation = computed(
+      () => ![MODEL_TYPE.META_BUILDING, MODEL_TYPE.PHOTOSPHERE_BUILDING].includes(props.model.type)
+    );
+
     const folderLocation = (model) => {
       const parentFolders = getAscendants(model, props.allFolders)
-        .map((f) => ({
-          id: f.id,
-          name: f.name,
-        }))
+        .map((f) => ({ id: f.id, name: f.name }))
         .reverse();
       return parentFolders;
     };
@@ -124,9 +134,6 @@ export default {
       const parentFolders = getAscendants(doc, props.allFolders);
       return parentFolders[0]?.name ?? t("t.rootFolder");
     };
-
-    const hovering = ref(false);
-    const showFullPath = ref(false);
 
     const selectFile = (folder) => {
       const selectedFolder = props.allFolders.find((f) => f.id === folder.id);
@@ -155,6 +162,7 @@ export default {
     };
 
     return {
+      hasLocation,
       hovering,
       showFullPath,
       goToGedRoot,
