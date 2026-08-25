1548a77b123093496fc7310112edca0f506ce9c6
Gaëtan Lagier <gaetan.lagier@hotmail.fr>
2026-08-25 15:51:31 +0200

    feat: allow to load 2D/3D or 3D beta if at least one is C

--- src/components/specific/models/models-table/model-actions-cell/ViewerButton.vue
+++ src/components/specific/models/models-table/model-actions-cell/ViewerButton.vue
@@ -1,27 +1,29 @@
 <template>
-  <AppLink
-    data-test-id="btn-open-viewer"
-    :data-test-param="window"
-    :disabled="disabled"
-    :to="{
-      name: routeNames.modelViewer,
-      params: {
-        spaceID: project.cloud.id,
-        projectID: project.id,
-        modelIDs: model.id,
-      },
-      query: {
-        window,
-      },
-    }"
-  >
-    <BIMDataButton :disabled="disabled" class="viewer-button" :color="color" outline radius icon>
-      <span>
-        {{ text || window.toUpperCase() }}
-      </span>
-      <slot name="subtext"> </slot>
-    </BIMDataButton>
-  </AppLink>
+  <BIMDataTooltip :text="disabled ? tooltip : ''" position="bottom">
+    <AppLink
+      data-test-id="btn-open-viewer"
+      :data-test-param="window"
+      :disabled="disabled"
+      :to="{
+        name: routeNames.modelViewer,
+        params: {
+          spaceID: project.cloud.id,
+          projectID: project.id,
+          modelIDs: model.id,
+        },
+        query: {
+          window,
+        },
+      }"
+    >
+      <BIMDataButton :disabled="disabled" class="viewer-button" :color="color" outline radius icon>
+        <span>
+          {{ text || window.toUpperCase() }}
+        </span>
+        <slot name="subtext"> </slot>
+      </BIMDataButton>
+    </AppLink>
+  </BIMDataTooltip>
 </template>
 
 <script setup>
@@ -50,6 +52,10 @@ defineProps({
   text: {
     type: String,
   },
+  tooltip: {
+    type: String,
+    default: "",
+  },
   color: {
     type: String,
     default: "granite",
@@ -62,7 +68,7 @@ defineProps({
   color: var(--color-granite-light);
 
   &:disabled {
-    visibility: hidden;
+    opacity: 0.4;
   }
 }
 </style>
